ab565c25ff5d129952a7e86061134fa55572b5a7
Reichard Cris <reichard@jdlife.co.nz>
2020-06-06 12:55:13 +0800

    Add search/Index.vue

---
+++ src/pages/Search/Index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+// import GalleryHorizontal from 'src/components/GalleryHorizontal'
+// import PopularCompanies from 'src/components/PopularCompanies'
+export default {
+  name: 'Search',
+  data () {
+    return {
+      slide: 1
+    }
+  },
+  components: {
+    // 'gallery-horizontal': GalleryHorizontal,
+    // 'popular-companies': PopularCompanies
+  }
+}
+</script>
+
+<style>
+
+</style>
