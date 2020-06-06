1e311d262c2e425a889d4cff45c4665d4a91f0db
Reichard Cris <reichard@jdlife.co.nz>
2020-06-06 13:27:32 +0800

    Add search tabs

--- src/pages/Search/Index.vue
+++ src/pages/Search/Index.vue
@@ -1,5 +1,115 @@
 <template>
-    <div></div>
+  <div class="">
+    <q-tabs
+        narrow-indicator
+        dense
+        align="justify"
+    >
+        <q-tab class="text-secondary" name="mails" label="업체명" />
+        <q-tab class="text-secondary" name="alarms" label="장소" />
+    </q-tabs>
+    <q-list bordered padding>
+      <q-item>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" />
+        </q-item-section>
+        <q-item-section>List item</q-item-section>
+        <q-item-section side>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>5 min ago</q-item-label>
+          <q-icon name="star" color="yellow" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar color="primary" text-color="white" square icon="bluetooth" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-badge label="10k" />
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced inset="item" />
+
+      <q-item>
+        <q-item-section top avatar>
+          <q-avatar rounded>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator spaced />
+
+      <q-item>
+        <q-item-section top thumbnail class="q-ml-none">
+          <img src="https://cdn.quasar.dev/img/mountains.jpg">
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>Single line item</q-item-label>
+          <q-item-label caption>Secondary line text. Lorem ipsum dolor sit amet, consectetur adipiscit elit.</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <q-item-label caption>meta</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </div>
 </template>
 
 <script>
