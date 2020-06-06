7bd031c1133fdf30171a7ac9713996937a317c26
Adem Reichard <reichardcris@gmail.com>
2020-06-06 18:21:41 +0800

    Update Search/Index.vue

--- src/pages/Search/Index.vue
+++ src/pages/Search/Index.vue
@@ -1,30 +1,15 @@
 <template>
   <div class="">
-    <div>
-        <q-input outlined placeholder="철거업체 통합검색">
-          <template v-slot:prepend>
-            <q-icon name="search" />
-          </template>
-        </q-input>
-    </div>
-    <q-tabs
-        narrow-indicator
-        dense
-        align="justify"
-    >
-        <q-tab class="text-secondary q-pa-sm" name="mails" label="업체명" />
-        <q-tab class="text-secondary q-pa-sm" name="alarms" label="장소" />
-    </q-tabs>
     <q-list padding class="q-mt-md">
       <template v-for="count in 20">
       <q-item :key="count">
         <q-item-section top avatar>
-          <q-avatar color="primary" text-color="white" icon="bluetooth" />
+          <q-img class="image" src="https://picsum.photos/360/360" width="70px" height="70px" />
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>원철거</q-item-label>
-          <q-item-label caption lines="2" class="text-black text-bold">서울특별시 광진구 천호대로.</q-item-label>
+          <q-item-label class="text-bold">원철거</q-item-label>
+          <q-item-label caption lines="2" class="text-bold" style="color: #454545">서울특별시 광진구 천호대로.</q-item-label>
           <q-item-label caption lines="2" class="text-bold" style="color: #999999">리뷰 510 . 매칭 1,120</q-item-label>
         </q-item-section>
 
@@ -33,15 +18,13 @@
           <q-item-label class="text-bold text-black text-center" caption>4.9</q-item-label>
         </q-item-section>
       </q-item>
-      <q-separator :key="count" spaced />
+      <q-separator :key="count + '-separator'" spaced />
       </template>
     </q-list>
   </div>
 </template>
 
 <script>
-// import GalleryHorizontal from 'src/components/GalleryHorizontal'
-// import PopularCompanies from 'src/components/PopularCompanies'
 export default {
   name: 'Search',
   data () {
@@ -50,8 +33,7 @@ export default {
     }
   },
   components: {
-    // 'gallery-horizontal': GalleryHorizontal,
-    // 'popular-companies': PopularCompanies
+
   }
 }
 </script>
