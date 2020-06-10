ce7c46ec50fd46fafd28b904471c745c2f43778d
Reichard Cris <reichard@jdlife.co.nz>
2020-06-10 17:46:52 +0800

    Add HeadingTabs.vue

---
+++ src/components/NavHeader/HeadingTabs.vue
@@ -0,0 +1,22 @@
+<template>
+    <q-tabs
+        v-model="tab"
+        inline-label
+        shrink
+        stretch
+        >
+            <q-tab v-for="tab in tabs" :key="tab.name" v-bind="tab" />
+    </q-tabs>
+</template>
+
+<script>
+export default {
+    name: "headingTabs",
+    data () {
+        return {
+
+        }
+    },
+}
+
+</script>
