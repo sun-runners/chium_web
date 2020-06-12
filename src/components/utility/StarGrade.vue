97b002b4c5c2677fc269487259fba8e5b0c0abf1
eevan7a9 <e7a9@protonmail.com>
2020-06-13 01:11:27 +0900

    update stargrade

--- src/components/utility/StarGrade.vue
+++ src/components/utility/StarGrade.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="row items-center">
     <div class="row items-center">
-      <q-icon class="q-pb-md" size="22px" name="star" color="secondary" v-for="n in star" :key="n" />
-      <p class="q-ml-sm text-weight-bold">{{star}}.0</p>
+      <q-rating
+        v-model="star"
+        size="20px"
+        :max="5"
+        color="secondary"
+      />
+      <p class="q-ml-sm q-pt-md text-weight-bold">{{star}}.0</p>
+      <!-- <q-icon class="q-pb-md" size="22px" name="star" color="secondary" v-for="n in star" :key="n" /> -->
+      <!-- <p class="q-ml-sm text-weight-bold">{{star}}.0</p> -->
     </div>
   </div>
 </template>
