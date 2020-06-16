f051a29f7b9d648af6d8242acfeecaf0a6369fc5
eevan7a9 <e7a9@protonmail.com>
2020-06-16 23:33:58 +0900

    added height to image

--- src/components/StoreWork/WorkItem.vue
+++ src/components/StoreWork/WorkItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="q-mx-sm q-pb-lg row justify-center">
     <div style="width: 500px;">
-        <q-img :src="post.img" native-context-menu></q-img>
+        <q-img height="256px" :src="post.img" native-context-menu></q-img>
         <p class="text-6 q-pt-md" style="font-size: 18px; line-height: 27px;">{{ post.title }}</p>
         <div class="row justify-start text-grey-14">
           <div  class="row"  v-for="(detail, index) in post.details" :key="index">
