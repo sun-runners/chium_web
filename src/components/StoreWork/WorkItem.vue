92c93e264b05b8d2701b3bec038c83afd694a52f
Reichard Cris <reichard@jdlife.co.nz>
2020-06-20 19:14:17 +0800

    Update WitkItem.vue

--- src/components/StoreWork/WorkItem.vue
+++ src/components/StoreWork/WorkItem.vue
@@ -1,17 +1,14 @@
-<template>
-  <div class="q-mx-sm q-pb-lg row justify-center">
-    <div style="width: 500px;">
-        <q-img height="256px" :src="post.img" native-context-menu></q-img>
-        <p class="text-6 q-pt-md" style="font-size: 18px; line-height: 27px;">{{ post.title }}</p>
-        <div class="row justify-start text-grey-14">
-          <div  class="row"  v-for="(detail, index) in post.details" :key="index">
-            <span>{{ detail }}</span>
-            <q-separator class="q-mx-sm" vertical size="2px"/>
-          </div>
-        </div>
-        <p class="q-pt-md text-secondary text-weight-bold">{{ post.price }}</p>
-    </div>
-  </div>
+<template lang="pug">
+  div(class='q-mx-sm q-pb-lg row justify-center')
+    div(style='width: 500px;')
+      q-img(height='256px', :src='post.img', native-context-menu='')
+      p(class='text-6 q-pt-md', style='font-size: 18px; line-height: 27px;') {{ post.title }}
+      div(class='row justify-start text-grey-14')
+        div(class='row', v-for='(detail, index) in post.details', :key='index')
+          span {{ detail }}
+          q-separator(class='q-mx-sm', vertical='', size='2px')
+      p(class='q-pt-md text-secondary text-weight-bold') {{ post.price }}
+
 </template>
 
 <script>
