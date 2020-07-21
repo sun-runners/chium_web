25a92aa3e40d4bab8a995e344996f4c4d9538705
Reichard Cris <reichard@jdlife.co.nz>
2020-07-21 10:04:30 +0800

    Update Search/Index.vue

--- src/pages/Search/Index.vue
+++ src/pages/Search/Index.vue
@@ -8,6 +8,7 @@
               q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-blue btn-group-filter btn-group-filter-active', flat label='전체')
               q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-grey-6 btn-group-filter', flat label='철거')
               q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-grey-6 btn-group-filter', flat label='폐기물')
+          
           div(class='col flex justify-end')
             q-btn-dropdown(color='white', class='text-black', flat='', label='인기순')
               q-list
@@ -20,17 +21,26 @@
                 q-item(clickable='', v-close-popup='', @click='onItemClick')
                   q-item-section
                     q-item-label Articles
+
       template(v-for='count in 20')
-        q-item(:key='count')
+        q-item(:key='count' class="result-list" clickable @click='onItemResultClick')
           q-item-section(top='', avatar='')
             q-img(class='image', src='https://picsum.photos/360/360', width='70px', height='70px')
           q-item-section
-            q-item-label(class='text-bold') &#xC6D0;&#xCCA0;&#xAC70;
-            q-item-label(caption='', lines='2', class='text-bold', style='color: #454545') &#xC11C;&#xC6B8;&#xD2B9;&#xBCC4;&#xC2DC; &#xAD11;&#xC9C4;&#xAD6C; &#xCC9C;&#xD638;&#xB300;&#xB85C;.
-            q-item-label(caption='', lines='2', class='text-bold', style='color: #999999') &#xB9AC;&#xBDF0; 510 . &#xB9E4;&#xCE6D; 1,120
+            q-item-label(class='notosanskr-medium' style="font-size: 15px") 미래 철거 폐기물
+            q-item-label(caption='', lines='2', class='notosanskr-regular', style='color: #454545; font-size: 13px') 경기 화성시 병점로 12
+            q-item-label(caption='', lines='2', class='text-bold', style='color: #999999; font-size: 12px') 
+              
+              span(class="review-matching-text-border")
+                span(class="text-secondary notosanskr-medium") 1,520 
+                span(class="notosanskr-regular") 리뷰
+              span(class="review-matching-text")
+                span(class="text-secondary notosanskr-medium") 12,520 
+                span(class="notosanskr-regular") 매칭
           q-item-section(side='', top='')
-            q-icon(name='star', color='secondary')
-              q-item-label(class='text-bold text-black text-center', caption='') 4.9
+            span
+              q-icon(name='star', color='secondary' style="font-size:20px")
+              q-item-label(class='notosanskr-medium score-value text-black text-center', caption='') 4.9
         q-separator(:key="count + '-separator'", spaced='')
 
 </template>
@@ -49,7 +59,10 @@ export default {
   methods: {
     onItemClick () {
       alert('click');
-    }
+    },
+    onItemResultClick () {
+      this.$router.push("store")
+    },
   }
 }
 </script>
@@ -64,4 +77,20 @@ export default {
 .btn-group-filter-active
   border: 1px solid #18A0FB
   color: #18A0FB
+
+.review-matching-text-border
+  padding: 10px
+  padding-left: 0px
+  border-right: 1px #F2F2F2 solid
+
+.review-matching-text
+  padding: 10px
+
+.result-list
+  cursor: pointer
+  &:hover
+   background-color: #F2F2F2
+
+.scope-value
+  font-size 12px
 </style>
