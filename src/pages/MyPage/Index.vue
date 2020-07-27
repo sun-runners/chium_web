3dd670da033cc1159e4d84c1dd3859249ea3c73d
Reichard Cris <reichard@jdlife.co.nz>
2020-07-27 18:33:48 +0800

    Update MyPage/Index.vue

--- src/pages/MyPage/Index.vue
+++ src/pages/MyPage/Index.vue
@@ -2,46 +2,46 @@
     <div class="bg-grey-3" style="min-height:100vh;">
         <div class="bg-white">
             <div class="name-point q-px-sm q-py-md column">
-                <p class="name q-ma-none">박가인 님</p>
-                <p class="point text-secondary q-ma-none">5,000P</p>
+                <p class="name q-ma-none notosanskr-medium">박가인 님</p>
+                <p class="point notosanskr-bold text-secondary q-ma-none">5,000P</p>
             </div>
             <div class="q-px-lg">
                 <q-separator color="grey-4" size="1px" />
             </div>
             <div class="row items-center justify-evenly">
                 <q-item-section class="review column items-center q-pa-lg">
-                    <p class=" text-dark q-ma-none" style="font-size: 24px;">1</p>
-                    <p class="text" style="font-size: 13px;">리뷰</p>
+                    <p class=" text-dark q-ma-none notosanskr-bold" style="font-size: 24px;">1</p>
+                    <p class="text notosanskr-medium" style="font-size: 13px;">리뷰</p>
                 </q-item-section>
                 <q-separator color="grey-4" vertical inset  size="1px" />
                 <q-item-section class="consult column items-center q-pa-lg">
-                    <p class=" text-dark q-ma-none" style="font-size: 24px;">2</p>
-                    <p class="text" style="font-size: 13px;">상담</p>
+                    <p class=" text-dark q-ma-none notosanskr-bold" style="font-size: 24px;">2</p>
+                    <p class="text notosanskr-medium" style="font-size: 13px;">상담</p>
                 </q-item-section>
             </div>
         </div>
         <div class="bg-white q-mt-md q-pa-none">
             <div class="row items-center justify-between">
                 <div class="col-8 q-pa-lg">
-                    <p class="text-grey q-ma-none">내 위치</p>
-                    <p class="q-ma-none q-pt-sm" style="font-size: 16px; line-height: 24px;">경기도 화성시 정남면 괘랑4길 16-38</p>
+                    <p class="text-grey q-ma-none notosanskr-medium">내 위치</p>
+                    <p class="q-ma-none q-pt-sm notosanskr-regular" style="font-size: 16px; line-height: 24px;">경기도 화성시 정남면 괘랑4길 16-38</p>
                 </div>
                 <div class="q-pa-lg ">
-                    <q-btn color="white" text-color="black" outline label="수정" />
+                    <q-btn color="white" text-color="black" class="notosanskr-medium" outline label="수정" />
                 </div>
             </div>
             <q-separator size="1px"/>
             <div class="row items-center justify-between">
                 <div class="col-8 q-pa-lg">
-                    <p class="q-ma-none text-grey">연락처</p>
-                    <p class="q-ma-none q-pt-sm" style="font-size: 16px; line-height: 24px;">010-2398-0374</p>
+                    <p class="q-ma-none text-grey notosanskr-medium">연락처</p>
+                    <p class="q-ma-none q-pt-sm notosanskr-bold" style="font-size: 16px; line-height: 24px;">010-2398-0374</p>
                 </div>
                 <div class="q-pa-lg ">
-                    <q-btn color="white" bold text-color="black"  outline label="수정"/>
+                    <q-btn color="white" class="notosanskr-medium" text-color="black"  outline label="수정"/>
                 </div>
             </div>
             <q-separator size="1px" />
-            <div class="q-pa-none q-pt-md q-mt-lg">
+            <div class="q-pa-none q-pt-md q-mt-lg notosanskr-medium">
                 <p style="font-size: 18px; line-height: 27px;" class=" q-pl-sm">최근 본 업체</p>
                 <gallery-sliding-horizontal></gallery-sliding-horizontal>
             </div>
@@ -82,9 +82,6 @@ export default {
 </script>
 
 <style scoped>
-p{
-    font-family: Noto Sans CJK KR;
-}
 
 .name-point .name{
     font-weight: 500;
