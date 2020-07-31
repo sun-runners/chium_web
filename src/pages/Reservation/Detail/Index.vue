c1898c2d7ec2522d5f2e99feca7c5dc1a9f264c7
eevan7a9 <e7a9@protonmail.com>
2020-07-31 13:23:58 +0900

    added Reservation Detail Demolition site info

--- src/pages/Reservation/Detail/Index.vue
+++ src/pages/Reservation/Detail/Index.vue
@@ -80,7 +80,7 @@
       </div>
     </div>
     <q-card flat class="my-card notosanskr-medium q-mb-md" id="requesterInfoCard">
-      <q-card-section class="row items-center q-px-lg">
+      <q-card-section class="row items-center q-px-md">
         <q-icon name="o_account_circle" class="text-grey-13 q-pr-sm" style="font-size: 34px;" />
         <div>의뢰자 정보</div>
       </q-card-section>
@@ -89,12 +89,36 @@
 
       <q-card-section class="column q-pb-none" style="font-size: 13px;">
         <div class="q-mb-sm">
-          <div class="text-grey-13 q-mb-xs">의뢰자</div>
+          <div class="text-grey-13 q-mb-xs" style="color: #888888;">의뢰자</div>
           <p style="color: #151515;">홍길동</p>
         </div>
         <div>
-          <div class="text-grey-13 q-mb-xs">연락처</div>
+          <div class="text-grey-13 q-mb-xs" style="color: #888888;">연락처</div>
           <p style="color: #151515;">010-2398-0374</p>
+        </div>
+      </q-card-section>
+    </q-card>
+
+    <q-card flat class="my-card notosanskr-medium q-mb-md" id="demolitionSiteCard">
+      <q-card-section class="row items-center q-px-md">
+        <q-icon name="o_place" class="text-grey-13 q-pr-sm" style="font-size: 34px;" />
+        <div>철거장소</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section class="column q-pb-none" style="font-size: 13px;">
+        <div class="q-mb-sm">
+          <div class="text-grey-13 q-mb-xs" style="color:#888888;">주소</div>
+          <p style="color: #151515;">경기도 화성시 괘랑4길 16-38번지 솔가타운하우스</p>
+        </div>
+        <div>
+          <div class="text-grey-13 q-mb-xs" style="color:#888888;">공간유형</div>
+          <p style="color: #151515;">주거공간/주택</p>
+        </div>
+        <div>
+          <div class="text-grey-13 q-mb-xs" style="color:#888888;">평수</div>
+          <p style="color: #151515;">30-40평</p>
         </div>
       </q-card-section>
     </q-card>
