0a79f989bbd02d9619d47e98faddf152ae404020
eevan7a9 <e7a9@protonmail.com>
2020-07-31 13:17:38 +0900

    added Reservation detail requesterInfo

--- src/pages/Reservation/Detail/Index.vue
+++ src/pages/Reservation/Detail/Index.vue
@@ -79,6 +79,25 @@
         />
       </div>
     </div>
+    <q-card flat class="my-card notosanskr-medium q-mb-md" id="requesterInfoCard">
+      <q-card-section class="row items-center q-px-lg">
+        <q-icon name="o_account_circle" class="text-grey-13 q-pr-sm" style="font-size: 34px;" />
+        <div>의뢰자 정보</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section class="column q-pb-none" style="font-size: 13px;">
+        <div class="q-mb-sm">
+          <div class="text-grey-13 q-mb-xs">의뢰자</div>
+          <p style="color: #151515;">홍길동</p>
+        </div>
+        <div>
+          <div class="text-grey-13 q-mb-xs">연락처</div>
+          <p style="color: #151515;">010-2398-0374</p>
+        </div>
+      </q-card-section>
+    </q-card>
   </div>
 </template>
 
