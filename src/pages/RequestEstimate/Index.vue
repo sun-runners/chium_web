a90807b1261a9a502c59f4fb31e5140756c1be17
eevan7a9 <e7a9@protonmail.com>
2020-08-13 00:14:30 +0900

    added process indicaor

--- src/pages/RequestEstimate/Index.vue
+++ src/pages/RequestEstimate/Index.vue
@@ -1,12 +1,24 @@
 <template>
   <div class>
     <div>Request Estimate</div>
+    <process-indicator :totalSteps="totalSteps" :activeStep="activeStep" :activeColor="activeColor"></process-indicator>
   </div>
 </template>
 
 <script>
+import ProcessIndicator from "components/Utility/ProcessIndicator";
 export default {
   name: "RequestEstimate",
+  components: {
+    "process-indicator": ProcessIndicator,
+  },
+  data() {
+    return {
+      totalSteps: 6,
+      activeStep: 1,
+      activeColor: "#55E2EB",
+    };
+  },
 };
 </script>
 
