4ee71f73413e5f9a4369d51d3456fc80af14ea06
eevan7a9 <e7a9@protonmail.com>
2020-08-13 19:53:17 +0900

    created NavHeaderInfo component

---
+++ src/components/Utility/NavHeaderInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-card class="my-card" flat>
+    <q-card-section class="row content-center" style="height: 44px;">
+      <q-btn flat color="dark" icon="keyboard_backspace" v-if="backIcon" />
+      <q-btn flat color="dark" icon="clear" v-else />
+      <div
+        style="font-weight: 500; font-size: 16px;"
+        class="notosanskr-regular col row items-center justify-center"
+      >{{ name }}</div>
+      <q-btn flat color="dark" icon="clear" v-if="backIcon" />
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    backIcon: Boolean,
+  },
+};
+</script>
+
+<style>
+</style>
