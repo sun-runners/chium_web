3bdd6f65e4a08551c6af9bf601c26aa1c4f164b7
eevan7a9 <e7a9@protonmail.com>
2020-08-16 01:51:02 +0900

    develop custom radio-check-icon component

---
+++ src/components/Utility/RadioCheckIcon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input
+      type="radio"
+      class="q-pa-none q-ma-none"
+      :name="name"
+      @change="$emit('change', value)"
+      :value="value"
+      v-if="value != currentValue"
+    />
+    <q-icon class="text-dark q-pa-none q-ma-none" size="30px" v-else>
+      <img src="~assets/radio_selected.svg" class="q-pa-none q-ma-none" />
+    </q-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    name: String,
+    currentValue: String,
+  },
+};
+</script>
+
+<style scoped>
+input[type="radio"] {
+  height: 30px;
+  width: 30px;
+}
+</style>
