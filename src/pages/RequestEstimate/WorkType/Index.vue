f121d87738d4de4e013224cccd2744bc7fe49d25
eevan7a9 <e7a9@protonmail.com>
2020-08-24 14:48:16 +0900

    added selection of work_type

--- src/pages/RequestEstimate/WorkType/Index.vue
+++ src/pages/RequestEstimate/WorkType/Index.vue
@@ -2,7 +2,8 @@
   <div class="bg-white notosanskr-regular">
     <div class="q-px-md">
       <div class="ask-work q-pa-none">
-        <span>작업 유형</span>을 선택해주세요.
+        <span>작업 유형</span>
+        을 선택해주세요.
       </div>
       <div class="select-work-type">
         <!-- -------------Home Space -->
@@ -39,6 +40,27 @@
         </div>
       </div>
     </div>
+    <!--  Tab panels starts here -->
+    <q-tab-panels v-model="workType" animated transition-next="slide-left" flat class>
+      <q-tab-panel name="demolition" class="q-pa-none">
+        <div style="background: #F2F2F2; height: 10px"></div>
+        <div class="q-px-md">
+          <div class="specific-type">철거 내용을 선택해주세요.</div>
+          <div class="q-py-md row">
+            <q-btn
+              unelevated
+              rounded
+              class="work-type-button"
+              :class="{'work-type-selected': selectedWorkType == item}"
+              v-for="(item, index) in demolitionType"
+              :key="index"
+              :label="item"
+              @click="workTypeSelected(item)"
+            />
+          </div>
+        </div>
+      </q-tab-panel>
+    </q-tab-panels>
   </div>
 </template>
 
@@ -51,7 +73,26 @@ export default {
   data() {
     return {
       workType: "",
+      selectedWorkType: "",
+      demolitionType: ["부분철거", "전체철거"],
     };
+  },
+  methods: {
+    workTypeSelected(value) {
+      this.selectedWorkType = value;
+      this.$emit("next", true);
+    },
+  },
+  watch: {
+    workType: function () {
+      if (this.workType == "waste_disposal") {
+        this.selectedWorkType = "waste_disposal";
+        this.$emit("next", true);
+      } else {
+        this.selectedWorkType = "";
+        this.$emit("next", false);
+      }
+    },
   },
 };
 </script>
@@ -74,10 +115,39 @@ export default {
     color: #46b3fc;
   }
 }
+
+.select-space-type {
+  padding: 35px 0px;
+  .space-type {
+    height: 44px;
+  }
+}
+
+.specific-type {
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 30px;
+  color: #888888;
+  padding-top: 35px;
+}
+
+.work-type-button {
+  background: #f1f1f1;
+  font-size: 14px;
+  line-height: 32px;
+  margin: 7.5px 5px;
+}
+
 .select-work-type {
   padding: 35px 0px;
   .work-type {
     height: 44px;
   }
+}
+
+.work-type-selected {
+  background: #46b3fc;
+  color: white;
 }
 </style>
