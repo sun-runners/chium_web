c74cc849632d77ede7702c8043c56ba5077f134c
Reichard Cris <reichard@jdlife.co.nz>
2020-08-24 11:14:06 +0800

    Update Home/Index.vue

--- src/pages/Home/Index.vue
+++ src/pages/Home/Index.vue
@@ -1,30 +1,28 @@
 <template lang="pug">
    div
-    q-banner(class='banner text-white no-margin' style="min-height:400px; background-color:#62BDF8;")
+    div(class='text-white no-margin' style="min-height:420px; background-color:#46B3FC;")
       div(class="row")
-        div(class="col flex justify-center items-center content-center")
-          q-img(:src="require('src/assets/banner_top.png')" class="" style="max-width: 375px; height: 100%; max-height: 400px")
-    div
-      div(class='row')
-        div(class='col')
-          div(class='row')
-            div(class='col')
-              h6(class='q-mt-md q-mb-xs q-ml-md text-black notosanskr-medium home-heading-text') 우리 동네 업체
-            div(class='col flex justify-end')
-              q-btn(flat class='q-mt-md q-mb-xs q-ml-md q-mr-lg text-black view-more-btn notosanskr-medium') 더보기
+        div(class="col" style="min-width: 228px;")
+          div(class="column")
+            p(class="col banner-text q-pt-xl q-pl-md") 치움은 
+              br
+              | 어떤일을 하나요?
+          
+            div(class="col")
+              q-btn(label="치움 가이드 보러가기" flat class="guide-btn q-ml-lg q-mt-md" size="sm" align="left" icon-right="arrow_forward_ios")
         
-      gallery-sliding-horizontal
-      
-      popular-companies
-
-      div(class='column')
-        div(class='q-pt-md')
-          q-banner(class='banner bg-secondary q-pa-none text-white')
-            q-img(:src="require('src/assets/banner_bottom.png')" style="max-width: 100%; height: 100%;")
-
-        div(class='bg-grey-3 q-py-lg')
-          home-notice
-          terms-of-service
+        div(class="col flex justify-end items-end content-end" style="min-height: 420px;")
+          q-img(:src="require('src/assets/boy.svg')" class="q-mr-sm" style="max-width: 250px; min-width: 250px; height: 100%; max-height: 268px")
+          
+    div(class="row bg-primary" style="min-height: 220px")
+      div(class="col q-pa-sm q-mt-md flex content-center justify-center")
+        q-btn( flat class="full-width full-height home-btn bg-white" align="center"  type="a" size="xl") 
+          q-icon(size="110px" :name="`img:${require('src/assets/icon_truck.svg')}`")
+          p 폐기물 버리기
+      div(class="col q-pa-sm q-mt-md flex content-center justify-center")
+        q-btn(type="a" align="center" flat class="full-width full-height home-btn bg-white" size="xl")
+          q-icon(size="110px" :name="`img:${require('src/assets/icon_swipe.svg')}`")
+          p 철거하기
 
 </template>
 
@@ -58,6 +56,31 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+
+.home-btn
+  min-height: 179px
+  max-width: 166px
+  font-family: 'notosanskr-regular'
+  font-size: 16px
+  line-height: 28px
+  letter-spacing: -0.9px
+
+.banner-text
+  font-family: 'notosanskr-medium'
+  font-size: 34px
+  text-align: left
+  min-width: 288px
+  // border: 1px solid #000
+
+
+.guide-btn
+  font-family: 'notosanskr-medium';
+  font-weight: 500
+  font-size: 14px
+  height 46px
+  border-radius: 1000px
+  border: 1px solid #fff
+
 .view-more-btn
   font-size: 13px
   border: 0.5px solid #D2D2D2
