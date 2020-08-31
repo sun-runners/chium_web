d5bef9a7f0c7d05fb1e1a53c74c8c67a55a23434
eevan7a9 <e7a9@protonmail.com>
2020-08-31 17:08:28 +0900

    added request-complete page

---
+++ src/pages/RequestEstimate/CompleteEstimate/Index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="bg-white notosanskr-regular">
+    <div class="q-px-md">
+      <div class="evaluate-complete q-pa-none">
+        <span>견적신청</span>
+        을 완료하세요.
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.evaluate-complete {
+  width: 129px;
+  font-size: 27px;
+  line-height: 40px;
+  /* or 148% */
+
+  font-style: normal;
+  font-weight: normal;
+  font-size: 27px;
+  line-height: 40px;
+  /* or 148% */
+  margin-top: 30px;
+  letter-spacing: -1.35px;
+  span {
+    color: #46b3fc;
+  }
+}
+</style>
