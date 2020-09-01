cc69d09ca9a6a14ffa211adab9a3cd7a4e3c9510
Reichard Cris <reichard@jdlife.co.nz>
2020-09-01 16:11:43 +0800

    add index.js

---
+++ src/components/TermConditionNotes/index.js
@@ -0,0 +1,10 @@
+import TermsOfService from './TermsOfService'
+import PrivacyPolicy from './PrivacyPolicy'
+import ReceivingMarketingInformation from './ReceivingMarketingInformation'
+
+
+export {
+  TermsOfService,
+  PrivacyPolicy,
+  ReceivingMarketingInformation
+};
