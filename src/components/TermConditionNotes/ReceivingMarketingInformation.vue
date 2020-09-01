a20009e3fe38b323c4707ee82fbc96a01f5495fa
Reichard Cris <reichard@jdlife.co.nz>
2020-09-01 16:25:43 +0800

    Add RecievingMarketingInformation.vue

---
+++ src/components/TermConditionNotes/ReceivingMarketingInformation.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+  q-card(flat)
+    q-card-section
+      p(class="title") 마케팅 정보 수신
+
+    q-card-section
+      p Chum 개인정보 취급 방침
+      span 뉴딩스 (이하"회사")는 이용자의 개인정보를 매우 중요하게 생각하며 정
+        | 보통신서비스 제공자가 준수하여야 하는 관련 법령 및 규정을 준수하고
+        | 있습니다. 본 개인정보취급방침은 SNECK(이하"서비스")에 적용되며,
+        | 다음과 같은 내용을 담고 있습니다.
+
+    q-card-section
+      p 1.개인정보의 수집,이용 목적
+      span 회사는 아래와 같은 목적으로 서비스 제공을 위한 최소한의 개인정보만
+      | 을 수집하며, 수집한 정보를 목적 외로 사용하거나, 이용자의 동의 없이
+      | 제3자에게 제공하는 등 외부에 공개하지 않습니다.
+
+    q-separator
+
+    q-card-section
+      p ① 서비스 제공에 관한 계약 이행 및 서비스 기능 구현
+      span 
+        b(class="text-black notosanskr-medium") ② 회원관리 :
+      span 회원제 서비스 제공에 따른 본인 확인, 개인식별, 가입의사
+        | 확인, 이용약관 위반 및 불량 회원의 부정 이용 방지와 차단, 민원처리, 고
+        | 지사항 전달등에 사용됩니다.
+
+      p
+      span
+        b(class="text-black notosanskr-medium") ③ 신규 서비스 개발 및 마케팅 광고에의 활용 : 
+      span 신규 서비스 개발 및 맞춤
+        | 서비스 제공, 통계학적 특성에 따른 서비스 제공 및 광고 게재, 서비스 유
+        | 효성 확인, 이벤트 및 광고성 정보 제공 및 참여기회 제공, 접속빈도 파악,
+        | 회원의 서비스 이용에 대한 통계
+
+    q-separator
+
+    q-card-section
+      p 2. 수집하는 개인정보 항목 및 수집방법
+      span 뉴딩스 (이하"회사")는 이용자의 개인정보를 매우 중요하게 생각하며 정
+        | 보통신서비스 제공자가 준수하여야 하는 관련 법령 및 규정을 준수하고
+        | 있습니다. 본 개인정보취급방침은 SNECK(이하"서비스")에 적용되며,
+        | 다음과 같은 내용을 담고 있습니다.
+        | 회사는 아래와 같은 목적으로 서비스 제공을 위한 최소한의 개인정보만
+        | 을 수집하며, 수집한 정보를 목적 외로 사용하거나, 이용자의 동의 없이
+        | 제3자에게 제공하는 등 외부에 공개하지 않습니다.
+        | 회사는 아래의 경우 개인정보를 수집합니다.
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+p
+  color: #000
+  font-size: 14px
+  font-family: 'notosanskr-medium'
+
+span
+  color: #757575
+  font-size: 13px
+  font-family: 'notosanskr-regular'
+
+.title
+  font-size: 27px
+  font-family: 'notosanskr-medium'
+</style>
