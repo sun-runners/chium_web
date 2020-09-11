720075e395e95ecc7e0c4ba4216e139931826a06
eevan7a9 <e7a9@protonmail.com>
2020-09-12 01:11:09 +0900

    created My Account page

---
+++ src/pages/MyAccount/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <default-wrapper>
+    <div class="bg-green ">
+      My Account Page
+    </div>
+  </default-wrapper>
+</template>
+
+<script>
+import DefaultWrapper from "src/components/PageWrappers/DefaultWrapper";
+export default {
+  components: {
+    "default-wrapper": DefaultWrapper,
+  },
+};
+</script>
+
+<style scoped></style>
