c078c1600d0e3c26d8fa0dbcd75740f40fa8555c
eevan7a9 <e7a9@protonmail.com>
2020-09-12 04:26:21 +0900

    moddified navtab

--- src/components/Utility/NavHeaderInfo.vue
+++ src/components/Utility/NavHeaderInfo.vue
@@ -1,13 +1,27 @@
 <template>
   <q-card class="my-card" flat>
     <q-card-section class="row content-center q-px-none" style="height: 44px;">
-      <q-btn flat color="dark" icon="keyboard_backspace" :to="{name: backTo}" v-if="backTo" />
-      <q-btn flat color="dark" icon="clear" :to="{name: closedTo}" v-else />
+      <q-btn
+        flat
+        color="dark"
+        icon="keyboard_backspace"
+        :to="{ name: backTo }"
+        v-if="backTo"
+      />
+      <q-btn flat color="dark" icon="clear" :to="{ name: closedTo }" v-else />
       <div
         style="font-weight: 500; font-size: 16px;"
         class="notosanskr-regular col row items-center justify-center"
-      >{{ name }}</div>
-      <q-btn flat color="dark" icon="clear" :to="{name: closedTo}" v-if="backTo" />
+      >
+        {{ name }}
+      </div>
+      <q-btn
+        flat
+        color="dark"
+        icon="clear"
+        :to="{ name: closedTo }"
+        v-if="backTo && closedTo"
+      />
       <div style="width: 56px" v-else></div>
     </q-card-section>
   </q-card>
@@ -23,5 +37,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
--- src/pages/MyAccount/Index.vue
+++ src/pages/MyAccount/Index.vue
@@ -1,16 +1,18 @@
 <template>
   <default-wrapper>
-    <div class="bg-green ">
-      My Account Page
+    <div class="bg-white" style="min-height: 100vh;">
+      <nav-header-info name="내 계정" backTo="my_page"  />
     </div>
   </default-wrapper>
 </template>
 
 <script>
 import DefaultWrapper from "src/components/PageWrappers/DefaultWrapper";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
 export default {
   components: {
     "default-wrapper": DefaultWrapper,
+    "nav-header-info": NavHeaderInfo,
   },
 };
 </script>
