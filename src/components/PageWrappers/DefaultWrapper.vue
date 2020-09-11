12e0ca16b86df53106c4e6997441308710e2f05f
eevan7a9 <e7a9@protonmail.com>
2020-09-12 01:10:39 +0900

    created page wrapper default wrapper

---
+++ src/components/PageWrappers/DefaultWrapper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-grey">
+    <q-layout view="hHh lpR fFf">
+      <q-page-container>
+        <slot :style="widthMax"></slot>
+      </q-page-container>
+    </q-layout>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DefaultWrapper",
+  data() {
+    return {};
+  },
+  computed: {
+    widthMax() {
+      return { width: window.innerWidth + "px", "max-width": "1000px" };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
