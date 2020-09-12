12fb4b303958e632a04fb2b3ccb65f69ac0fc419
eevan7a9 <e7a9@protonmail.com>
2020-09-12 12:49:05 +0900

    added account name & apple-logo

--- src/pages/MyAccount/Index.vue
+++ src/pages/MyAccount/Index.vue
@@ -1,7 +1,18 @@
 <template>
   <default-wrapper>
     <div class="bg-white" style="min-height: 100vh;">
-      <nav-header-info name="내 계정" backTo="my_page"  />
+      <nav-header-info name="내 계정" backTo="my_page" />
+      <div class="account notosanskr-regular">
+        <small>계정</small>
+        <div style="padding-top: 7px;">
+          <q-icon class="text-dark" size="20px" style="margin-right: 10px;">
+            <img src="~assets/apple_link.svg" />
+          </q-icon>
+          <span class="name"> </span>
+          애플 계정 연동
+        </div>
+      </div>
+      <q-separator style="margin-left: 16px;"></q-separator>
     </div>
   </default-wrapper>
 </template>
@@ -17,4 +28,20 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.account {
+  padding: 20px 16px 15px;
+  small {
+    font-size: 13px;
+    line-height: 19px;
+    letter-spacing: -0.65px;
+    color: #767676;
+  }
+  .name {
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.8px;
+    color: #151515;
+  }
+}
+</style>
