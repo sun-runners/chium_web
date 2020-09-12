3d633ea6b8463d5dfbcb69a983f724b28bf896cb
eevan7a9 <e7a9@protonmail.com>
2020-09-12 13:12:28 +0900

    added username section

--- src/pages/MyAccount/Index.vue
+++ src/pages/MyAccount/Index.vue
@@ -2,14 +2,22 @@
   <default-wrapper>
     <div class="bg-white" style="min-height: 100vh;">
       <nav-header-info name="내 계정" backTo="my_page" />
+      <!-- Account Info Section -->
       <div class="account notosanskr-regular">
         <small>계정</small>
         <div style="padding-top: 7px;">
           <q-icon class="text-dark" size="20px" style="margin-right: 10px;">
             <img src="~assets/apple_link.svg" />
           </q-icon>
-          <span class="name"> </span>
-          애플 계정 연동
+          <span class="name"> 애플 계정 연동</span>
+        </div>
+      </div>
+      <q-separator style="margin-left: 16px;"></q-separator>
+      <!-- Name Section -->
+      <div class="username notosanskr-regular">
+        <small>이름</small>
+        <div style="padding-top: 5px;" class="name">
+          홍길동
         </div>
       </div>
       <q-separator style="margin-left: 16px;"></q-separator>
@@ -29,7 +37,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.account {
+.account,
+.username {
   padding: 20px 16px 15px;
   small {
     font-size: 13px;
