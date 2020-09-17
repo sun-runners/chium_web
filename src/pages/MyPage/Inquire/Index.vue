0a968abdd3fdcbdab298755fcce1b5ab4b983407
eevan7a9 <e7a9@protonmail.com>
2020-09-18 03:17:35 +0900

    added header tab

---
+++ src/pages/MyPage/Inquire/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <default-template :styleSlots="styleSlots">
+    <template #pageHeader>
+      <nav-header-info name="업체문의" class="full-width" backTo="my_page" />
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "InterestedCompany",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {
+      styleSlots: {
+        bgHeader: "white",
+        bgBody: "#F3F3F3",
+        bgFooter: "",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
