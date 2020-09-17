5236c10f7a5f05e4cf3d5e3a4e1e6e96cff508f6
eevan7a9 <e7a9@protonmail.com>
2020-09-18 08:26:19 +0900

    added inquiry answer-message & action buttns

--- src/pages/MyPage/Inquire/Index.vue
+++ src/pages/MyPage/Inquire/Index.vue
@@ -6,16 +6,23 @@
 
     <template #pageBody>
       <div class="q-py-sm">
-        <q-card flat bordered class="my-card" style="margin-bottom: 10px">
+        <q-card
+          flat
+          bordered
+          class="my-card"
+          style="margin-bottom: 10px"
+          v-for="(inquiry, index) in inquiries"
+          :key="index"
+        >
           <!-- company Name, State -->
           <q-card-section
             class="row justify-between items-center notosanskr-medium q-py-none"
           >
             <div class="company-name">
-              원철거
+              {{ inquiry.name }}
             </div>
             <div class="company-state">
-              <div class="state-text">미답변</div>
+              <div class="state-text">{{ inquiry.state }}</div>
             </div>
           </q-card-section>
 
@@ -24,16 +31,16 @@
           <q-card-section>
             <div class="info-section ">
               <div class="info-item">
-                <span class="q-mr-sm">경력</span>11년
-                <!-- {{ company.career }} -->
+                <span class="q-mr-sm">경력</span>
+                {{ inquiry.career }}
               </div>
               <div class="info-item">
-                <span class="q-mr-sm">매칭</span>1,102회
-                <!-- {{ company.matching }} -->
+                <span class="q-mr-sm">매칭</span>
+                {{ inquiry.matching }}
               </div>
               <div class="info-item text-secondary">
-                <span class="q-mr-sm">평점</span>4.0
-                <!-- {{ company.rating }} -->
+                <span class="q-mr-sm">평점</span>
+                {{ inquiry.grade }}
               </div>
             </div>
           </q-card-section>
@@ -49,18 +56,40 @@
             </div>
             <div class="question-section">
               <div class="question-message">
-                100평이상 공간 철거비용 문의합니다.
+                {{ inquiry.question.message }}
               </div>
               <div class="question-date">
-                2020-04-02
+                {{ inquiry.question.date }}
+              </div>
+            </div>
+          </q-card-section>
+          <q-separator inset v-if="inquiry.answer" />
+          <q-card-section
+            class="row items-center answer-section"
+            v-if="inquiry.answer"
+          >
+            <div
+              class="row justify-center items-center bg-secondary text-white"
+              style="height: 36px; width: 36px; border-radius: 50%;0"
+            >
+              A
+            </div>
+            <div class="col ">
+              <div class="answer-message notosanskr-medium">
+                {{ inquiry.answer }}
               </div>
             </div>
           </q-card-section>
 
           <q-separator inset />
           <!-- buttons section -->
-          <q-card-section>
-            s
+          <q-card-section class="row notosanskr-medium">
+            <button class="col btn-modify" v-if="!inquiry.answer">
+              수정하기
+            </button>
+            <button class="col btn-delete">
+              삭제하기
+            </button>
           </q-card-section>
         </q-card>
       </div>
@@ -84,6 +113,46 @@ export default {
         bgBody: "#F3F3F3",
         bgFooter: "",
       },
+      inquiries: [
+        {
+          name: "원철거",
+          state: "미답변",
+          career: "11년",
+          matching: "1,102회",
+          grade: "4.0",
+          question: {
+            message: "100평이상 공간 철거비용 문의합니다.",
+            date: "2020-08-01 [비밀글]",
+          },
+          answer: null,
+        },
+        {
+          name: "혜성인철거",
+          state: "미답변",
+          career: "11년",
+          matching: "1,102회",
+          grade: "4.0/5",
+          question: {
+            message: "100평이상 공간 철거비용 문의합니다.",
+            date: "2020-08-01 [비밀글]",
+          },
+          answer:
+            "안녕하세요 고객님 답변이 늦어져서 죄송합니다. 자세한 비용은 저희 업체에 전화해주시길 부탁드립니다. 감사합니다.",
+        },
+        {
+          name: "혜성인철거",
+          state: "미답변",
+          career: "11년",
+          matching: "1,102회",
+          grade: "4.0/5",
+          question: {
+            message: "100평이상 공간 철거비용 문의합니다.",
+            date: "2020-08-01 [비밀글]",
+          },
+          answer:
+            "안녕하세요 고객님 답변이 늦어져서 죄송합니다. 자세한 비용은 저희 업체에 전화해주시길 부탁드립니다. 감사합니다.",
+        },
+      ],
     };
   },
 };
@@ -139,5 +208,26 @@ export default {
     line-height: 18px;
     color: #767676;
   }
+}
+.answer-section {
+  background: #2cb3ff1c;
+  padding-top: 15px;
+  padding-bottom: 16px;
+  .answer-message {
+    padding-left: 10px;
+    font-size: 13px;
+    line-height: 20px;
+    letter-spacing: -0.65px;
+    color: #151515;
+  }
+}
+.btn-modify,
+.btn-delete {
+  height: 40px;
+  border: 1px solid #d6d6d6;
+  box-sizing: border-box;
+  border-radius: 4px;
+  background: white;
+  margin: auto 4px;
 }
 </style>
