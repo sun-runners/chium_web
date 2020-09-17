98cb3901417216f0f9a4c8983b93ed7114a2404e
eevan7a9 <e7a9@protonmail.com>
2020-09-18 03:42:12 +0900

    added copany name, state

--- src/pages/MyPage/Inquire/Index.vue
+++ src/pages/MyPage/Inquire/Index.vue
@@ -3,6 +3,42 @@
     <template #pageHeader>
       <nav-header-info name="업체문의" class="full-width" backTo="my_page" />
     </template>
+
+    <template #pageBody>
+      <div class="q-py-sm">
+        <q-card flat bordered class="my-card" style="margin-bottom: 10px">
+          <!-- company Name, State -->
+          <q-card-section
+            class="row justify-between items-center notosanskr-medium q-py-none"
+          >
+            <div class="company-name">
+              원철거
+            </div>
+            <div class="company-state">
+              <div class="state-text">미답변</div>
+            </div>
+          </q-card-section>
+
+          <q-separator inset />
+          <!-- company info -->
+          <q-card-section>
+            s
+          </q-card-section>
+
+          <q-separator inset />
+          <!-- company Q & A -->
+          <q-card-section>
+            s
+          </q-card-section>
+
+          <q-separator inset />
+          <!-- buttons section -->
+          <q-card-section>
+            s
+          </q-card-section>
+        </q-card>
+      </div>
+    </template>
   </default-template>
 </template>
 
@@ -27,4 +63,27 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.company-name {
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 44px;
+  letter-spacing: -0.7px;
+  color: #151515;
+}
+.company-state {
+  height: 24px;
+  width: 51px;
+  background: #2cb3ff1c;
+  border-radius: 100px;
+
+  .state-text {
+    font-weight: 500;
+    font-size: 13px;
+    line-height: 20px;
+    text-align: center;
+    letter-spacing: -0.65px;
+    color: #2cb3ff;
+  }
+}
+</style>
