76e193b51ed3467b1fc7ec5bfd41884c210d174f
eevan7a9 <e7a9@protonmail.com>
2020-09-19 11:13:12 +0900

    added timeline icons and structure of schedule

--- src/pages/MyPage/WorkSchedule/Index.vue
+++ src/pages/MyPage/WorkSchedule/Index.vue
@@ -5,8 +5,23 @@
     </template>
 
     <template #pageBody>
-      <div class="q-py-sm">
-        content
+      <div class="q-py-sm row">
+        <div class="vertical-line"><div class="vl"></div></div>
+        <div class="content-container col">
+          <div class="schedule">
+            <q-icon class="text-dark" size="20px" style="margin-right: 10px;">
+              <img src="~assets/timeline_ongoing.svg" />
+            </q-icon>
+            <q-card>
+              <q-card-section>
+                {{ body }}
+              </q-card-section>
+            </q-card>
+          </div>
+          <div class="bg-grey-13">
+            {{ body }}
+          </div>
+        </div>
       </div>
     </template>
   </default-template>
@@ -28,9 +43,30 @@ export default {
         bgBody: "#F3F3F3",
         bgFooter: "",
       },
+      body:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     };
   },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.vertical-line {
+  // background: rgba(121, 255, 128, 0.486);
+  min-height: 100vh;
+  width: 46px;
+  .vl {
+    border-left: 1px dashed #959595;
+    min-height: inherit;
+    margin-left: 23px;
+  }
+}
+.schedule {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: -30px;
+  padding-right: 16px;
+  margin-bottom: 16px;
+}
+</style>
