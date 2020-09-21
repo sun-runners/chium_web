330bda40cc6f8fb378210a12716277574575d53b
eevan7a9 <e7a9@protonmail.com>
2020-09-22 07:57:09 +0900

    created Notice Page

---
+++ src/pages/MyPage/Notice/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <default-template>
+    <template #pageHeader>
+      <nav-header-info name="공지사항" class="full-width" backTo="my_page" />
+    </template>
+
+    <template #pageBody>
+      <div>
+        notice page
+      </div>
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "WorkSchedule",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
