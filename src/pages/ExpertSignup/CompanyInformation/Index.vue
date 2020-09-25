ad2492becce264df88973bde7bdcc49122bc3947
eevan7a9 <e7a9@protonmail.com>
2020-09-25 16:18:58 +0900

    created Company Basic Information Page

---
+++ src/pages/ExpertSignup/CompanyInformation/Index.vue
@@ -0,0 +1,54 @@
+<template>
+  <default-template :styleSlots="styleSlots">
+    <template #pageBody>
+      <div class="bg-white">
+        <div class="q-pa-sm">
+          company Basic Information
+        </div>
+      </div>
+    </template>
+    <template #pageFooter>
+      <q-btn
+        class="full-width notosanskr-regular btn-footer"
+        style="font-size:17px;"
+        :rounded="false"
+        flat
+        label="다음"
+      />
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "TermsCondition",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {
+      styleSlots: {
+        bgBody: "white",
+        bgFooter: "white",
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn-footer {
+  color: #919698;
+  background: #e8eaeb;
+  border-radius: 0;
+  height: 60px;
+}
+
+.btn-ready {
+  color: #ffffff;
+  background: #46b3fc;
+}
+</style>
