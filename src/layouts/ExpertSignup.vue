567befbfe79a18a51e5a32e4f1d5c05d543332bb
eevan7a9 <e7a9@protonmail.com>
2020-09-25 12:31:19 +0900

    updated Expertsignup top header nav

--- src/layouts/ExpertSignup.vue
+++ src/layouts/ExpertSignup.vue
@@ -1,17 +1,42 @@
 <template>
-  <div class="bg-grey">
-    <q-layout view="hHh lpR fFf" class="bg-red">
+  <div class="bg-white">
+    <q-layout view="hHh lpR fFf">
       <!-- Header Starts here -->
-      <q-header
-        class="text-dark row justify-center bg-grey-11"
-        height-hint="98"
-      >
-        <div :style="widthMax" class="bg-white">
-          <nav-header-info
-            name="전문가 가입"
-            :backTo="returnTo"
-            closedTo="store"
-          ></nav-header-info>
+      <q-header class="text-dark row justify-center bg-white" height-hint="98">
+        <div :style="widthMax">
+          <q-card class="my-card" flat>
+            <q-card-section
+              class="row content-center q-px-none"
+              style="height: 44px; padding: 0 16px;"
+            >
+              <router-link
+                :to="{ name: returnTo }"
+                class="row items-center"
+                style="text-decoration:none; width: 56px;"
+              >
+                <q-icon
+                  class="text-dark"
+                  name="keyboard_backspace"
+                  size="24px"
+                />
+              </router-link>
+
+              <div
+                style="font-weight: 500; font-size: 16px;"
+                class="notosanskr-medium col row items-center justify-center  nav-text"
+              >
+                전문가 가입
+              </div>
+              <q-btn
+                flat
+                color="dark"
+                icon="clear"
+                :to="{ name: closedTo }"
+                v-if="returnTo && closedTo"
+              />
+              <div style="width: 56px" v-else></div>
+            </q-card-section>
+          </q-card>
           <div class="q-px-sm">
             <process-indicator
               :totalSteps="totalSteps"
@@ -22,10 +47,7 @@
         </div>
       </q-header>
       <!-- Child Routes content here            -->
-      <q-page-container
-        class="row justify-center bg-grey-11"
-        style="min-height:100vh;"
-      >
+      <q-page-container class="row justify-center" style="min-height:100vh;">
         <router-view
           class="q-ma-none q-pa-none"
           style="background: #F2F2F2"
@@ -52,6 +74,7 @@ export default {
       activeColor: "",
       redirectTo: "",
       returnTo: "",
+      closedTo: "",
     };
   },
   computed: {
@@ -66,7 +89,7 @@ export default {
           stepNum: 1,
           btnReady: false,
           redirect: "floor_space",
-          returnTo: "",
+          returnTo: "my_page",
         },
       ];
     },
