2785fb441ec87c19c29864a6767b47ec66bed94f
eevan7a9 <e7a9@protonmail.com>
2020-09-28 23:14:10 +0900

    created service area page

---
+++ src/pages/ExpertSignup/ServiceArea/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <default-template :styleSlots="styleSlots">
+    <template #pageBody>
+      <div class="bg-white">
+        <div class="q-px-md q-pt-sm">
+          <div class="title notosanskr-regular">업체주소</div>
+        </div>
+      </div>
+    </template>
+    <template #pageFooter>
+      <q-btn
+        class="full-width notosanskr-regular btn-footer btn-ready"
+        style="font-size:17px;"
+        :rounded="false"
+        flat
+        label="다음"
+      />
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "ServiceArea",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {
+      styleSlots: {
+        bgBody: "white",
+        bgFooter: "white",
+      },
+    };
+  },
+  methods: {},
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 27px;
+  line-height: 40px;
+  letter-spacing: -1.35px;
+  color: #151515;
+}
+
+.btn-footer {
+  color: #919698;
+  background: #e8eaeb;
+  border-radius: 0;
+  height: 60px;
+}
+
+.btn-ready {
+  color: #ffffff;
+  background: #46b3fc;
+}
+</style>
