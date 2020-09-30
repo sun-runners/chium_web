e3461013d98c8049ca8778b9d2c033811869032a
eevan7a9 <e7a9@protonmail.com>
2020-09-30 17:48:40 +0900

    created proof Company page

---
+++ src/pages/ExpertSignup/ProofCompany/Index.vue
@@ -0,0 +1,74 @@
+<template>
+  <default-template :styleSlots="styleSlots">
+    <template #pageBody>
+      <div class="bg-white">
+        <div class="q-px-md q-pt-sm">
+          <div class="title notosanskr-regular">업체증빙</div>
+          <!-- service selection section -->
+        </div>
+      </div>
+    </template>
+    <template #pageFooter>
+      <q-btn
+        class="full-width notosanskr-regular btn-footer "
+        style="font-size:17px;"
+        :rounded="false"
+        flat
+        label="가입신청 완료"
+      />
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "ProofCompany",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {
+      styleSlots: {
+        bgBody: "white",
+        bgFooter: "white",
+      },
+    };
+  },
+  methods: {
+    selectItem(item) {
+      if (!this.selectedRepresentative.includes(item)) {
+        this.selectedRepresentative.push(item);
+      } else {
+        this.selectedRepresentative = this.selectedRepresentative.filter(
+          (service) => service != item
+        );
+      }
+    },
+  },
+  watch: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.title {
+  font-size: 27px;
+  line-height: 40px;
+  letter-spacing: -1.35px;
+  color: #151515;
+}
+
+.btn-footer {
+  color: #919698;
+  background: #e8eaeb;
+  border-radius: 0;
+  height: 60px;
+}
+
+.btn-ready {
+  color: #ffffff;
+  background: #46b3fc;
+}
+</style>
