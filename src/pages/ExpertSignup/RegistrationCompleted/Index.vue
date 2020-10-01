8674e710bd0fcec447983fac70279b8382fdfff3
eevan7a9 <e7a9@protonmail.com>
2020-10-01 19:54:32 +0900

    added registration completed page

---
+++ src/pages/ExpertSignup/RegistrationCompleted/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <dir>
+    Registrtion Completed
+  </dir>
+</template>
+
+<script>
+export default {
+  name: "RegistrationCompleted",
+};
+</script>
+
+<style lang="scss" scoped></style>
