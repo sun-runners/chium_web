5f2755e197eb168d413e556b4e75e12812cd1593
Reichard Cris <reichard@jdlife.co.nz>
2020-10-04 11:17:48 +0800

    Modal

---
+++ src/components/Utility/BottomModal.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  q-dialog(v-model="modal" position="bottom")
+</template>
+
+<script>
+import ModalMixins from 'src/mixins/ModalMixins'
+
+export default {
+  mixins:['ModalMixins'],
+  inject: ['openModal'],
+  created() {
+    // this.modal = this.openModal;
+  },
+  watch: {
+    openModal: {
+      deep: true,
+      handler(newVal) {
+        console.log("newVal", newVal)
+      },
+    }
+  },
+}
+</script>
