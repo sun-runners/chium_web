76c742dd5a7942c0d51ef8ac8991a60cc644b451
eevan7a9 <e7a9@protonmail.com>
2020-10-21 15:10:29 +0900

    enable toggle pickFile

--- src/components/Utility/CustomFileUpload.vue
+++ src/components/Utility/CustomFileUpload.vue
@@ -3,36 +3,32 @@
     <q-file
       outlined
       class="field"
-      ref="myFileSelector"
+      ref="filepicker"
       :label="label"
       v-model="myFile"
-      v-if="!myFile"
+      v-show="!myFile"
     >
       <template v-slot:append>
         <div class="attachment notosanskr-medium">첨부</div>
       </template>
     </q-file>
-    <q-file
-      outlined
-      class="field"
-      :class="{'have-content': myFile}"
-      ref="myFileSelector"
-      v-model="myFile"
-      v-else
+
+    <div
+      class="picked-file row justify-between items-center"
+      @click="toggleFilePicker"
+      v-show="myFile"
     >
-      <template v-slot:prepend>
-        <q-icon name="o_photo_size_select_actual" />
-      </template>
-      <template v-slot:append>
-        <div class="attachment notosanskr-medium">첨부</div>
-      </template>
-    </q-file>
+      <q-icon name="o_photo_size_select_actual" color="secondary" />
+      <div class="q-pl-sm col text-secondary">{{ myFile ? myFile.name : ''}}</div>
+
+      <div class="attachment notosanskr-medium">첨부</div>
+    </div>
   </div>
 </template>
 
 <script>
 /**
- * event @changed will emit the file selected
+ * event emit @changed will emit the file selected
  */
 export default {
   name: "CustomFileUploader",
@@ -43,6 +39,11 @@ export default {
   },
   props: {
     label: "",
+  },
+  methods: {
+    toggleFilePicker() {
+      this.$refs.filepicker.pickFiles();
+    },
   },
   watch: {
     myFile() {
@@ -57,10 +58,6 @@ export default {
   background: #ffffff;
   box-sizing: border-box;
   border-radius: 8px;
-  &.have-content {
-    background: #2cb2ff4d;
-    color: red;
-  }
   .attachment {
     font-weight: 500;
     font-size: 16px;
@@ -69,5 +66,10 @@ export default {
     letter-spacing: -0.8px;
     color: #195de4;
   }
+}
+.picked-file {
+  padding: 18px;
+  background: #2cb2ff4d;
+  cursor: pointer;
 }
 </style>
