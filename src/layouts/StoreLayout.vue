3e32231b44e723be6cb6d9e3499dc64e99c44ca6
eevan7a9 <e7a9@protonmail.com>
2020-10-23 17:49:45 +0900

    created Store layout

---
+++ src/layouts/StoreLayout.vue
@@ -0,0 +1,57 @@
+<template>
+  <default-template :styleSlots="styleSlots">
+    <template #pageHeader>
+      <nav-header-info name="작업일정" class="full-width" backTo="my_page" />
+      <q-tabs
+        v-model="filterTabs"
+        class="text-black notosanskr-regular"
+        active-color="secondary"
+        dense
+        align="justify"
+      >
+        <q-route-tab
+          name="store_detail"
+          :to="{ name: 'store_detail' }"
+          class="filterTab"
+          label="업체소개"
+        />
+        <q-route-tab
+          name="reviews"
+          :to="{ name: 'store_review' }"
+          class="filterTab"
+          label="후기(633)"
+        />
+        <q-route-tab name="inquire" :to="{ name: 'store_inquiry' }" class="filterTab" label="문의하기" />
+      </q-tabs>
+    </template>
+
+    <template #pageBody>
+      <router-view></router-view>
+    </template>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
+import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+export default {
+  name: "StoreLayout",
+  components: {
+    "default-template": DefaultTemplate,
+    "nav-header-info": NavHeaderInfo,
+  },
+  data() {
+    return {
+      styleSlots: {
+        bgHeader: "white",
+        bgBody: "#F3F3F3",
+        bgFooter: "",
+      },
+      filterTabs: "view_all",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
