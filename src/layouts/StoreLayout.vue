ba7ae63d2357faa7e8457626a9c326078bd47fd9
eevan7a9 <e7a9@protonmail.com>
2020-10-27 00:20:30 +0900

    updated header title

--- src/layouts/StoreLayout.vue
+++ src/layouts/StoreLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <default-template :styleSlots="styleSlots">
     <template #pageHeader>
-      <nav-header-info name="작업일정" class="full-width" backTo="search" />
+      <nav-header-info :name="'어게인업'" class="full-width" backTo="search" />
       <q-tabs
         v-model="filterTabs"
         class="text-black notosanskr-regular"
