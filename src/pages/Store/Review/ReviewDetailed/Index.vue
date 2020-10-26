8e8ebdfc01366b7b024eedf7f46bbab3b4a3291a
eevan7a9 <e7a9@protonmail.com>
2020-10-26 17:40:38 +0900

    created Store review detailed

---
+++ src/pages/Store/Review/ReviewDetailed/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="min-height: 100vh; background: #F3F3F3;">
+    <div class="header bg-white row items-center q-pl-md" style="height: 44px">
+      <q-icon name="close" size="24px"></q-icon>
+    </div>
+
+    <div class="bg-white q-ma-md">
+      <q-img src="https://placeimg.com/500/300/nature" :ratio="1" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ReviewDetailed",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
