bdfa2bace64d4a7df39ced04eba4ae582c0ea786
eevan7a9 <e7a9@protonmail.com>
2020-10-26 23:50:48 +0900

    updated fix for browser view

--- src/pages/Store/Review/ReviewDetailed/Index.vue
+++ src/pages/Store/Review/ReviewDetailed/Index.vue
@@ -1,40 +1,52 @@
 <template>
-  <div style="min-height: 100vh; background: #F3F3F3;">
-    <div class="header bg-white row items-center q-pl-xs" style="height: 44px">
-      <q-btn flat round color="black" icon="close" size="16px" :to="{name:'store_review'}" />
-    </div>
-
-    <div class="bg-white q-ma-md notosanskr-regular" style="padding-bottom: 27px;">
-      <q-img src="https://placeimg.com/500/300/nature" :ratio="1" />
-      <div class="details-container q-px-md">
-        <div class="name">박*인</div>
-        <q-rating
-          v-model="score"
-          class="rating-size q-pa-none q-ma-none"
-          :max="5"
-          color="secondary"
-          size="20px"
-        />
-        <div class="details q-pa-none q-ma-none row">
-          <div v-for="(item, index) in details" :key="index" class="row q-pa-none">
-            <div class="text-grey-14">{{ item }}</div>
-            <q-separator vertical size="2px" class="q-mx-sm" />
+  <default-template :styleSlots="styleSlots">
+    <template #pageHeader>
+      <div class="header bg-white row items-center q-pl-xs" style="height: 44px">
+        <q-btn flat round color="black" icon="close" size="16px" :to="{name:'store_review'}" />
+      </div>
+    </template>
+    <template #pageBody>
+      <div class="bg-white q-ma-md notosanskr-regular" style="padding-bottom: 27px;">
+        <q-img src="https://placeimg.com/500/300/nature" :ratio="1" />
+        <div class="details-container q-px-md">
+          <div class="name">박*인</div>
+          <q-rating
+            v-model="score"
+            class="rating-size q-pa-none q-ma-none"
+            :max="5"
+            color="secondary"
+            size="20px"
+          />
+          <div class="details q-pa-none q-ma-none row">
+            <div v-for="(item, index) in details" :key="index" class="row q-pa-none">
+              <div class="text-grey-14">{{ item }}</div>
+              <q-separator vertical size="2px" class="q-mx-sm" />
+            </div>
           </div>
         </div>
+        <div
+          class="message q-px-md"
+          style="padding-top: 13px;"
+        >가게를 처음하는거라 어떻게 해야할지 막막했어요 ㅠㅠ 인테리어부터 싹뜯어내고 다시해야할거 같아서 철거업체에 의뢰를 하여 진행했습니다. 너무나 안전하게 시공날짜에 맞춰서 신속하게 진행해줘서 감사했어요 ㅎㅎ 덕분에 가게 인테리어도 기분좋게 끝냈습니다.</div>
       </div>
-      <div
-        class="message q-px-md"
-        style="padding-top: 13px;"
-      >가게를 처음하는거라 어떻게 해야할지 막막했어요 ㅠㅠ 인테리어부터 싹뜯어내고 다시해야할거 같아서 철거업체에 의뢰를 하여 진행했습니다. 너무나 안전하게 시공날짜에 맞춰서 신속하게 진행해줘서 감사했어요 ㅎㅎ 덕분에 가게 인테리어도 기분좋게 끝냈습니다.</div>
-    </div>
-  </div>
+    </template>
+  </default-template>
 </template>
 
 <script>
+import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
 export default {
   name: "ReviewDetailed",
+  components: {
+    "default-template": DefaultTemplate,
+  },
   data() {
     return {
+      styleSlots: {
+        bgHeader: "white",
+        bgBody: "#F3F3F3",
+        bgFooter: "",
+      },
       score: 4,
       details: ["주거공간", "오피스텔", "12평", "서울시 종로구"],
     };
