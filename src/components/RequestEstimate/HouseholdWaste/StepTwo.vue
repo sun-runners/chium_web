a96cd29702be92ccdd07873b614c569b20445e11
eevan7a9 <e7a9@protonmail.com>
2020-11-18 04:58:33 +0900

    created stepTwo for waste household

---
+++ src/components/RequestEstimate/HouseholdWaste/StepTwo.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <p class="heading-title">
+      폐기물 유형
+      <span class="heading-subtitle">을 알려주세요.</span>
+    </p>
+    <div class="row">
+      <div class="col">
+        <q-list separator="separator">
+          <q-item
+            class="q-pt-lg q-pb-lg q-pl-none"
+            clickable="clickable"
+            v-for="(list, key) in list_household_waste_type"
+            :key="key"
+            @click="selectedWaste = list.val; $emit('next', true);"
+          >
+            <q-item-section avatar="avatar">
+              <q-icon>
+                <img :src="require(`assets/request_estimate-icon/${list.icon1}`)" />
+              </q-icon>
+            </q-item-section>
+            <q-item-section
+              style="font-size: 16px; font-family: notosanskr-regular"
+            >{{ list.label }}</q-item-section>
+            <q-item-section avatar="avatar">
+              <q-icon>
+                <img :src="require(`assets/request_estimate-icon/medium_arrow.right.svg`)"
+                />
+              </q-icon>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedWaste: "",
+      list_household_waste_type: [
+        {
+          icon1: "bed.svg",
+          label: "가구",
+          val: "domestic",
+        },
+        {
+          icon1: "radio.svg",
+          label: "가전",
+          val: "industrial",
+        },
+        {
+          icon1: "clothes.svg",
+          label: "생활용품",
+          val: "construction",
+        },
+        {
+          icon1: "document.svg",
+          label: "기타",
+          val: "box",
+        },
+      ],
+    };
+  },
+  computed: {},
+  methods: {
+    myIcon() {
+      return;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.heading-title {
+  font-size: 27px;
+  font-family: "notosanskr-regular";
+}
+.heading-subtitle {
+  color: #959595;
+}
+</style>
