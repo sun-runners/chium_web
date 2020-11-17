bbee0436463d9b561bb0a65d703092057f85a7dc
eevan7a9 <e7a9@protonmail.com>
2020-11-18 02:03:49 +0900

    updated steps & updated Navigation toolbar

--- src/layouts/RequestEstimate.vue
+++ src/layouts/RequestEstimate.vue
@@ -4,32 +4,78 @@
       <!-- Header Starts here -->
       <q-header class="text-dark row justify-center bg-white" height-hint="98">
         <div :style="widthMax" class="bg-white">
-          <nav-header-info name="견적신청" :backTo="returnTo" closedTo="store"></nav-header-info>
+          <q-card class="my-card notosanskr-medium" flat>
+            <q-card-section
+              class="row content-center q-px-none"
+              style="height: 44px; padding: 0 16px;"
+            >
+              <router-link
+                :to="{ name: '' }"
+                v-if="activeStep > 1"
+                class="row items-center"
+                style="text-decoration:none; width: 56px;"
+              >
+                <q-icon
+                  class="text-dark"
+                  name="keyboard_backspace"
+                  size="24px"
+                />
+              </router-link>
+              <router-link
+                :to="{ name: '' }"
+                class="row items-center"
+                style="text-decoration:none"
+                v-else
+              >
+                <q-icon class="text-dark" name="clear" size="24px" />
+              </router-link>
+              <div
+                style="font-weight: 500; font-size: 16px;"
+                class="col row items-center justify-center  nav-text"
+              >
+                견적신청
+              </div>
+              <q-btn
+                flat
+                color="dark"
+                :to="{ name: 'my_page' }"
+                v-if="activeStep > 1"
+                label="취소"
+              />
+              <div style="width: 56px" v-else></div>
+            </q-card-section>
+          </q-card>
+          <!-- Progress of Process Starts here -->
           <div class="q-px-sm">
             <process-indicator
               :totalSteps="totalSteps"
               :activeStep="activeStep"
-              :activeColor="activeColor"
             ></process-indicator>
           </div>
         </div>
       </q-header>
       <!-- Child Routes content here            -->
-      <q-page-container class="row justify-center bg-white" style="min-height:100vh;">
+      <q-page-container
+        class="row justify-center bg-white"
+        style="min-height:100vh;"
+      >
         <router-view
           @next="setBtnReady"
           class="q-ma-none q-pa-none bg-white"
           style="background: #F2F2F2"
           :style="widthMax"
-          @onHandleStep="_ => activeStep = _"
+          @onHandleStep="(_) => (activeStep = _)"
         />
       </q-page-container>
       <!-- Request Footer content here -->
       <q-footer class="row justify-center bg-white" v-if="!isBtnHidden">
-        <q-toolbar class="bg-white text-white q-py-sm q-px-none" :style="widthMax">
+        <q-toolbar
+          class="bg-white text-white q-py-sm q-px-none"
+          :style="widthMax"
+        >
           <q-btn
             class="full-width notosanskr-regular btn-footer"
-            :class="{'btn-ready' : isBtnReady }"
+            :class="{ 'btn-ready': isBtnReady }"
             :disable="!isBtnReady"
             style="font-size:17px;"
             :rounded="false"
@@ -46,7 +92,7 @@
 <script>
 import NavHeaderInfo from "components/Utility/NavHeaderInfo";
 import ProcessIndicator from "components/Utility/ProcessIndicator";
-import { toInteger } from 'lodash';
+import { toInteger } from "lodash";
 
 export default {
   name: "RequestEstimate",
@@ -58,12 +104,9 @@ export default {
     return {
       totalSteps: 6,
       activeStep: 1,
-      activeColor: "",
       btnLabel: "다음",
       isBtnReady: false,
       isBtnHidden: false,
-      redirectTo: "",
-      returnTo: "",
       requestComplete: false,
     };
   },
@@ -74,53 +117,30 @@ export default {
     stepsList() {
       return [
         {
-          routeName: "space_type",
-          indicatorColor: "#55E2EB",
           stepNum: 1,
           btnReady: false,
-          redirect: "floor_space",
-          returnTo: "",
         },
         {
-          routeName: "floor_space",
-          indicatorColor: "#46B3FC",
           stepNum: 2,
           btnReady: false,
-          redirect: "work_type",
-          returnTo: "space_type",
         },
         {
-          routeName: "work_type",
-          indicatorColor: "#3A81EC",
           stepNum: 3,
           btnReady: false,
-          redirect: "due_date",
-          returnTo: "floor_space",
         },
         {
-          routeName: "due_date",
-          indicatorColor: "#495AEB",
           stepNum: 4,
           btnReady: false,
-          redirect: "picture_space",
-          returnTo: "work_type",
           hideBtnFooter: true,
         },
         {
           routeName: "picture_space",
-          indicatorColor: "#754BEB",
           stepNum: 5,
           btnReady: true,
-          redirect: "complete_estimate",
-          returnTo: "due_date",
         },
         {
-          routeName: "complete_estimate",
-          indicatorColor: "#2F00BE",
           stepNum: 6,
           btnReady: true,
-          redirect: "store",
-          returnTo: "picture_space",
         },
       ];
     },
@@ -136,26 +156,18 @@ export default {
     },
     setProcessIndicator() {
       const found_route = this.stepsList.find(
-        (step) => step.routeName == this.$route.name
+        (step) => step.stepNum == this.$route.params.step
       );
       if (found_route) {
         this.isBtnReady = found_route.btnReady;
         this.activeStep = found_route.stepNum;
-        this.activeColor = found_route.indicatorColor;
-        this.returnTo = found_route.returnTo;
-        if (found_route.redirect) {
-          this.redirectTo = found_route.redirect;
-          this.requestComplete = false;
-        } else {
-          this.redirectTo = "";
-          this.requestComplete = true;
-        }
+
         this.isBtnHidden = found_route.hideBtnFooter ? true : false;
       }
     },
     myBtnFunction() {
-      const { params: { step } } = this.$route;
-      this.$router.push(`/request/${toInteger(step)+1}`);
+      // we get the current step and we move to the next
+      this.$router.push(`/request/${toInteger(this.$route.params.step) + 1}`);
     },
   },
   watch: {
@@ -163,12 +175,12 @@ export default {
       this.isBtnReady = false;
       this.setProcessIndicator();
     },
-    '$route.params.step': {
+    "$route.params.step": {
       deep: true,
       handler(newStep) {
         this.activeStep = toInteger(newStep);
-      }
-    }
+      },
+    },
   },
   created() {
     this.setProcessIndicator();
