e3c8972a6ea8624f085c5e09878c1f94888a9ac3
eevan7a9 <e7a9@protonmail.com>
2020-11-18 04:58:58 +0900

    updated dynamic components sync

--- src/pages/Request/Index.vue
+++ src/pages/Request/Index.vue
@@ -1,55 +1,45 @@
 <template>
   <div class="q-mt-lg row">
     <div class="col-12">
-      <q-tab-panels v-model="$route.params.step">
-        <q-tab-panel name="1">
-          <component :is="currentStep"> </component>
-        </q-tab-panel>
-      </q-tab-panels>
+      <component :is="component" @next="$emit('next', true)"></component>
     </div>
   </div>
 </template>
 
 <script>
 import StepOne from "src/components/RequestEstimate/StepOne";
+import HouseholdWasteStepTwo from "src/components/RequestEstimate/HouseholdWaste/StepTwo";
 export default {
   components: {
     StepOne,
+    HouseholdWasteStepTwo,
   },
   data() {
     return {
-      currentStep: "StepOne",
-      steps: "1",
-      list_household_waste_type: [
-        {
-          icon1: "bed.svg",
-          label: "가구",
-          val: "domestic",
-        },
-        {
-          icon1: "radio.svg",
-          label: "가전",
-          val: "industrial",
-        },
-        {
-          icon1: "clothes.svg",
-          label: "생활용품",
-          val: "construction",
-        },
-        {
-          icon1: "document.svg",
-          label: "기타",
-          val: "box",
-        },
-      ],
+      component: "StepOne",
     };
   },
   mounted() {
     this.$emit("onHandleStep", this.$route.params.step);
   },
   methods: {},
+  watch: {
+    "$route.params.step": {
+      deep: true,
+      handler(newStep) {
+        switch (parseInt(newStep)) {
+          case 2:
+            this.component = "HouseholdWasteStepTwo";
+            break;
+
+          default:
+            this.component = "StepOne";
+            break;
+        }
+      },
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
