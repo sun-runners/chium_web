f43317ad8e268370e908cfddf3f50bde413e7c28
eevan7a9 <e7a9@protonmail.com>
2020-11-19 14:28:52 +0900

    separated searchModal to it's own component

---
+++ src/components/RequestEstimate/HouseholdWaste/SearchModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <q-dialog
+    v-model="dialog"
+    position="bottom"
+    transition-show="slide-up"
+    class="q-pa-none"
+  >
+    <q-card>
+      <q-card-section class="header notosanskr-medium">
+        가전 항목 0개 선택
+      </q-card-section>
+      <q-card-section class="q-py-none notosanskr-regular">
+        <q-input
+          v-model="searchText"
+          filled
+          bottom-slots
+          color="secondary"
+          label="냉장고, 건조기 등 검색"
+          label-color="grey-14"
+          :input-class="{ 'search-field': true }"
+        >
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+          <template v-slot:append>
+            <q-icon
+              name="close"
+              @click="searchText = ''"
+              class="cursor-pointer"
+            />
+          </template>
+        </q-input>
+      </q-card-section>
+      <q-separator></q-separator>
+      <q-card-section class="row items-center no-wrap q-pa-none">
+        Hello
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+  props: {
+    dialog: Boolean
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.q-card {
+  width: 100%;
+  max-width: 1000px;
+  margin: 0px;
+  margin-top: 50px;
+  min-height: 93vh;
+  background: white;
+  .header {
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.8px;
+    color: #151515;
+  }
+}
+</style>
--- src/components/RequestEstimate/HouseholdWaste/StepTwo.vue
+++ src/components/RequestEstimate/HouseholdWaste/StepTwo.vue
@@ -33,37 +33,20 @@
       </div>
     </div>
     <!-- Modal Starts Here -->
-    <q-dialog v-model="dialog" position="bottom" transition-show="slide-up" class="q-pa-none">
-      <q-card>
-        <q-card-section class="header notosanskr-medium">
-          가전 항목 0개 선택
-        </q-card-section>
-        <q-card-section class="q-py-none notosanskr-regular">
-          <q-input v-model="searchText" filled bottom-slots color="secondary" label="냉장고, 건조기 등 검색" label-color="grey-14" :input-class="{'search-field' : true}" :dense="dense">
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-            <template v-slot:append>
-              <q-icon name="close" @click="searchText = ''" class="cursor-pointer" />
-            </template>
-          </q-input>
-        </q-card-section>
-        <q-separator></q-separator>
-        <q-card-section class="row items-center no-wrap q-pa-none">
-          Hello
-        </q-card-section>
-      </q-card>
-    </q-dialog>
+    <search-modal :dialog="dialog"></search-modal>
   </div>
 </template>
 
 <script>
+import SearchModal from './SearchModal.vue';
 export default {
+  components:{
+    "search-modal": SearchModal
+  },
   data() {
     return {
       dialog: false,
       selectedWaste: "",
-      searchText: '',
       list_household_waste_type: [
         {
           icon1: "bed.svg",
@@ -104,20 +87,5 @@ export default {
 }
 .heading-subtitle {
   color: #959595;
-}
-
-.q-card {
-  width: 100%;
-  max-width: 1000px;
-  margin: 0px;
-  margin-top: 50px;
-  min-height: 93vh;
-  background: white;
-  .header {
-    font-size: 16px;
-    line-height: 24px;
-    letter-spacing: -0.8px;
-    color: #151515;
-  }
 }
 </style>
