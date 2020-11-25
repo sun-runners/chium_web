a7214873fe4eef61e72ff639e9ca0a05f6f6611e
eevan7a9 <e7a9@protonmail.com>
2020-11-25 12:14:20 +0900

    created house hold step two page

---
+++ src/pages/Request/Waste/StepTwo/household.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="q-pl-md q-pr-xs">
+    <p class="heading-title">
+      폐기할 물품
+      <span class="heading-subtitle">을 알려주세요.</span>
+    </p>
+    <div class="row">
+      <div class="col">
+        <p>content here</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.heading-title {
+  font-size: 27px;
+  font-family: "notosanskr-regular";
+}
+.heading-subtitle {
+  color: #959595;
+}
+</style>
