7c3acb271974457108482bf70f5f5c87ef7f5c81
eevan7a9 <e7a9@protonmail.com>
2020-11-27 18:40:48 +0900

    created visit options component

---
+++ src/components/RequestEstimate/HouseholdWaste/StepThree/VisitOptions.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-card flat>
+    <q-card-section class="q-py-none">
+      <div v-for="(option, i) in visitsOptions" :key="i">
+        <q-btn
+          flat
+          align="left"
+          class="options-text full-width items-center notosanskr-regular"
+          style="height: 64px;"
+          @click="selectedOption = option"
+        >
+          <q-icon style="margin-right: 20px" v-if="selectedOption == option">
+            <img src="~assets/request_estimate-icon/radio-btn-selected.png" />
+          </q-icon>
+          <q-icon style="margin-right: 20px" v-else>
+            <img src="~assets/request_estimate-icon/radio-btn-unselected.png" />
+          </q-icon>
+          {{ option }}
+        </q-btn>
+        <q-separator inset="" v-if="i == 0"></q-separator>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visitsOptions: ["예", "아니오"],
+      selectedOption: "예",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.options-text {
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.8px;
+  color: #15161a;
+}
+</style>
