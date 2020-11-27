8e1743c8260f75b8bc56c5874a0224f4be10b5c4
eevan7a9 <e7a9@protonmail.com>
2020-11-27 18:02:22 +0900

    created: Step 3 household page

---
+++ src/pages/Request/Waste/StepThree/household.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="">
+    <p class="heading-title bg-white q-ma-none q-px-md notosanskr-regular">
+      현장 사전방문
+      <span class="heading-subtitle">을 원하시나요?</span>
+    </p>
+    <div class="column full-height ">
+      content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {
+    layoutState() {
+      return {
+        totalSteps: 6,
+        activeStep: 3,
+        btnLabel: "다음",
+        isBtnReady: false,
+        isBtnHidden: false,
+        requestComplete: false,
+        nextPathRoute: "/request/waste",
+        prevPathRoute: "/request/waste/two/household",
+      };
+    },
+  },
+  mounted() {
+    this.$emit("setStateLayout", this.layoutState);
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.heading-title {
+  font-size: 27px;
+  line-height: 38px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -1.35px;
+  color: #15161a;
+  padding-bottom: 36px;
+}
+.heading-subtitle {
+  color: #959595;
+}
+</style>
