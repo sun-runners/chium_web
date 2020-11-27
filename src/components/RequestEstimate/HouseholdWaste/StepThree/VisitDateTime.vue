67b90792ee4ad0fb632a0b18745531ced14e9600
eevan7a9 <e7a9@protonmail.com>
2020-11-27 19:37:29 +0900

    created: visit Date time component

---
+++ src/components/RequestEstimate/HouseholdWaste/StepThree/VisitDateTime.vue
@@ -0,0 +1,84 @@
+<template>
+  <q-card flat class="q-mt-md bg-white">
+    <q-card-section class="q-pa-none  notosanskr-medium">
+      <q-input
+        filled
+        v-model="date"
+        bg-color="white"
+        color="secondary"
+        placeholder="방문 날짜"
+        class="date-field"
+        mask="date"
+        :rules="['date']"
+      >
+        <template v-slot:append>
+          <q-icon class="cursor-pointer">
+            <div class="select">선택</div>
+            <q-popup-proxy
+              ref="qDateProxy"
+              transition-show="scale"
+              transition-hide="scale"
+            >
+              <q-date v-model="date" color="secondary">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+      <q-separator inset />
+      <q-input
+        filled
+        bg-color="white"
+        color="secondary"
+        v-model="time"
+        placeholder="시간"
+        mask="time"
+        :rules="['time']"
+        class="time-field"
+      >
+        <template v-slot:append>
+          <q-icon class="cursor-pointer">
+            <div class="select">선택</div>
+            <q-popup-proxy transition-show="scale" transition-hide="scale">
+              <q-time v-model="time" color="secondary" text-color="black">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat />
+                </div>
+              </q-time>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      time: "",
+      date: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.date-field,
+.time-field {
+  padding-bottom: 0px;
+}
+.select {
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: right;
+  letter-spacing: -0.8px;
+  color: #195de4;
+}
+</style>
