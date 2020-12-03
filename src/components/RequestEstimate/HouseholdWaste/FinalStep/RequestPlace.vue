c3acb8fedc89861328fe848c235bfdd37d8e8c88
eevan7a9 <e7a9@protonmail.com>
2020-12-03 16:17:31 +0900

    perf(RequestPlace): update padding & radius

--- src/components/RequestEstimate/HouseholdWaste/FinalStep/RequestPlace.vue
+++ src/components/RequestEstimate/HouseholdWaste/FinalStep/RequestPlace.vue
@@ -4,15 +4,15 @@
       <div class="section-title notosanskr-medium">의뢰 장소</div>
     </q-card-section>
     <q-card-section
-      class="bg-white q-pa-md notosanskr-regular"
+      class="q-pa-none notosanskr-regular"
       style="margin: 12px 16px"
     >
       <!-- place-discharge 배출장소 -->
-      <div>
+      <div class="bg-white q-pa-md border-top-rad" style="margin-bottom: 2px">
         <div class="heading-text notosanskr-medium">
           배출장소
         </div>
-        <div class="row q-pt-sm" style="padding-bottom: 18px">
+        <div class="row q-pt-sm">
           <div class="text col">
             경기도 치움시 철거동 123-4 치움 번지 폐기 타운하우스 112호
           </div>
@@ -21,12 +21,13 @@
           </div>
         </div>
       </div>
+
       <!-- type 유형 -->
-      <div>
+      <div class="bg-white q-pa-md" style="margin-bottom: 2px">
         <div class="heading-text notosanskr-medium">
           유형
         </div>
-        <div class="row q-pt-sm" style="padding-bottom: 18px">
+        <div class="row q-pt-sm">
           <div class="text col">
             상업공간
           </div>
@@ -35,12 +36,13 @@
           </div>
         </div>
       </div>
+
       <!-- elevator 엘레베이터 -->
-      <div>
+      <div class="bg-white q-pa-md" style="margin-bottom: 2px">
         <div class="heading-text notosanskr-medium">
           엘레베이터
         </div>
-        <div class="row q-pt-sm" style="padding-bottom: 18px">
+        <div class="row q-pt-sm">
           <div class="text col">
             있음
           </div>
@@ -49,8 +51,11 @@
           </div>
         </div>
       </div>
+
       <!-- caution -->
-      <div class="caution notosanskr-medium">
+      <div
+        class="bg-white q-pa-md caution notosanskr-medium border-bottom-rad "
+      >
         <span>[주의] </span>업체 방문시 알려주신 정보와 실제 정보가 차이가
         발생할 경우 예상 견적비용과 실제 작업비용이 차이가 날 수 있습니다.
       </div>
@@ -82,7 +87,7 @@ export default {};
   letter-spacing: -0.65px;
   color: #767676;
 }
-.text-info {
+.text {
   font-size: 15px;
   line-height: 24px;
   letter-spacing: -0.75px;
@@ -107,5 +112,13 @@ export default {};
   span {
     color: #2cb3ff;
   }
+}
+.border-top-rad {
+  border-top-left-radius: 8px;
+  border-top-right-radius: 8px;
+}
+.border-bottom-rad {
+  border-bottom-left-radius: 8px;
+  border-bottom-right-radius: 8px;
 }
 </style>
