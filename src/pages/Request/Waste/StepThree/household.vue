e828006713bfdc6f6b757d14c10833ab178b70ff
eevan7a9 <e7a9@protonmail.com>
2020-12-05 04:09:21 +0900

    perf(household): added undecided option

--- src/pages/Request/Waste/StepThree/household.vue
+++ src/pages/Request/Waste/StepThree/household.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <p class="heading-title bg-white q-ma-none q-px-md notosanskr-regular">
-      현장 사전방문
-      <span class="heading-subtitle">을 원하시나요?</span>
+      작업 예정일
+      <span class="heading-subtitle">을 알려주세요.</span>
     </p>
-    <q-card class="my-card q-ma-md" flat :bordered="false">
+    <q-card class="my-card q-ma-sm" flat :bordered="false">
       <q-card-section style="height: 430px;" class="q-pa-none">
         <custom-calendar
           ref="calendar"
@@ -13,6 +13,18 @@
         ></custom-calendar>
       </q-card-section>
     </q-card>
+    <q-btn
+      class="schedule-undecided full-width bg-white notosanskr-regular q-py-sm"
+      align="between"
+      flat
+      @click="undecided = !undecided"
+    >
+      작업 예정일 미정
+      <q-icon>
+        <img src="~assets/circle_check_grey.svg" v-if="!undecided" />
+        <img src="~assets/circle_check_blue.svg" v-else />
+      </q-icon>
+    </q-btn>
   </div>
 </template>
 
@@ -26,6 +38,7 @@ export default {
     return {
       dateSelected: null,
       calendarVisible: true,
+      undecided: false,
     };
   },
   computed: {
@@ -70,5 +83,11 @@ export default {
 }
 .heading-subtitle {
   color: #959595;
+}
+.schedule-undecided {
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.8px;
+  color: #15161a;
 }
 </style>
