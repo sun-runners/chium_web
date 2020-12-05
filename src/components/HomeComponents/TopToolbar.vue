6a48d35b130194c00aac5821675dc71c2f3e4991
eevan7a9 <e7a9@protonmail.com>
2020-12-06 03:18:17 +0900

    perf(Home): created Toptoolbar

---
+++ src/components/HomeComponents/TopToolbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center bg-white">
+    <div class="flex items-center justify-center">
+      <span class="q-py-sm text-dark text-center notosanskr-medium">
+        괘랑4길 16-38
+      </span>
+      <q-btn
+        icon="arrow_drop_down_circle"
+        dense
+        color="secondary"
+        rounded
+        flat
+      ></q-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
