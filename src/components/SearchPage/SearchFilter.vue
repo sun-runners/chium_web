974bfb9ef3a2dfbaae6cc7d9093d64335aeb498c
eevan7a9 <e7a9@protonmail.com>
2020-12-06 02:58:40 +0900

    ref(searchpage): restructure

---
+++ src/components/SearchPage/SearchFilter.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="bg-white">
+    <q-input
+      filled
+      borderless
+      placeholder="철거업체 통합검색"
+      class="full-width"
+    >
+      <template v-slot:prepend>
+        <q-icon name="search" />
+      </template>
+      <template v-slot:append>
+        <q-icon name="close" @click="text = ''" class="cursor-pointer" />
+      </template>
+    </q-input>
+    <q-tabs inline-label dense align="justify" class="q-mt-sm bg-white">
+      <q-route-tab
+        class="text-secondary q-pa-sm"
+        v-for="tab in tabs"
+        :key="tab.name"
+        :name="tab.name"
+        v-bind="tab"
+        :to="{ name: tab.routeName }"
+      />
+    </q-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tabs: [
+        { name: "company_name", label: "전국" },
+        { name: "place1", label: "서울시" },
+        { name: "place2", label: "경기도" },
+        { name: "place3", label: "인천" },
+        { name: "place4", label: "대구" },
+        { name: "place5", label: "경남/경북" },
+        { name: "place6", label: "전라/충청" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/Search/Index.vue
+++ src/pages/Search/Index.vue
@@ -1,70 +1,144 @@
-<template lang="pug">
-  div(class='bg-grey-3 padding-top')
-    q-list(padding='', class='q-mt-lg bg-white')
-      q-item-label(header='')
-        div(class='row')
-          div(class='col')
-            q-btn-group(flat)
-              q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-blue btn-group-filter btn-group-filter-active', flat label='전체')
-              q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-grey-6 btn-group-filter', flat label='철거')
-              q-btn(color='white', :size="$q.screen.width < 376 ? 'xs' : 'sm'" class='text-grey-6 btn-group-filter', flat label='폐기물')
-          
-          div(class='col flex justify-end')
-            q-btn-dropdown(color='white', class='text-black', flat='', label='인기순')
-              q-list
-                q-item(clickable='', v-close-popup='', @click='onItemClick')
-                  q-item-section
-                    q-item-label Photos
-                q-item(clickable='', v-close-popup='', @click='onItemClick')
-                  q-item-section
-                    q-item-label Videos
-                q-item(clickable='', v-close-popup='', @click='onItemClick')
-                  q-item-section
-                    q-item-label Articles
-
-      template(v-for='count in 20')
-        q-item(:key='count' class="result-list" clickable @click='onItemResultClick')
-          q-item-section(top='', avatar='')
-            q-img(class='image', src='https://picsum.photos/360/360', width='70px', height='70px')
-          q-item-section
-            q-item-label(class='notosanskr-medium' style="font-size: 15px") 미래 철거 폐기물
-            q-item-label(caption='', lines='2', class='notosanskr-regular', style='color: #454545; font-size: 13px') 경기 화성시 병점로 12
-            q-item-label(caption='', lines='2', class='text-bold', style='color: #999999; font-size: 12px') 
-              
-              span(class="review-matching-text-border")
-                span(class="text-secondary notosanskr-medium") 1,520 
-                span(class="notosanskr-regular") 리뷰
-              span(class="review-matching-text")
-                span(class="text-secondary notosanskr-medium") 12,520 
-                span(class="notosanskr-regular") 매칭
-          q-item-section(side='', top='')
-            span
-              q-icon(name='star', color='secondary' style="font-size:20px")
-              q-item-label(class='notosanskr-medium score-value text-black text-center', caption='') 4.9
-        q-separator(:key="count + '-separator'", spaced='')
-
+<template>
+  <div class="bg-grey-3 padding-top">
+    <!-- search filter section -->
+    <search-filter />
+    <!-- section ends -->
+    <q-list class="q-mt-lg bg-white" padding="">
+      <q-item-label header="">
+        <div class="row">
+          <div class="col">
+            <q-btn-group flat="flat">
+              <q-btn
+                class="text-blue btn-group-filter btn-group-filter-active"
+                color="white"
+                :size="$q.screen.width &lt; 376 ? 'xs' : 'sm'"
+                flat="flat"
+                label="전체"
+              ></q-btn>
+              <q-btn
+                class="text-grey-6 btn-group-filter"
+                color="white"
+                :size="$q.screen.width &lt; 376 ? 'xs' : 'sm'"
+                flat="flat"
+                label="철거"
+              ></q-btn>
+              <q-btn
+                class="text-grey-6 btn-group-filter"
+                color="white"
+                :size="$q.screen.width &lt; 376 ? 'xs' : 'sm'"
+                flat="flat"
+                label="폐기물"
+              ></q-btn>
+            </q-btn-group>
+          </div>
+          <div class="col flex justify-end">
+            <q-btn-dropdown
+              class="text-black"
+              color="white"
+              flat=""
+              label="인기순"
+            >
+              <q-list>
+                <q-item clickable="" v-close-popup="" @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Photos</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable="" v-close-popup="" @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Videos</q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable="" v-close-popup="" @click="onItemClick">
+                  <q-item-section>
+                    <q-item-label>Articles</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </div>
+        </div> </q-item-label
+      ><template v-for="count in 20"
+        ><q-item
+          class="result-list"
+          :key="count"
+          clickable="clickable"
+          @click="onItemResultClick"
+          ><q-item-section top="" avatar="">
+            <q-img
+              class="image"
+              src="https://picsum.photos/360/360"
+              width="70px"
+              height="70px"
+            ></q-img>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label class="notosanskr-medium" style="font-size: 15px">
+              미래 철거 폐기물
+            </q-item-label>
+            <q-item-label
+              class="notosanskr-regular"
+              caption=""
+              lines="2"
+              style="color: #454545; font-size: 13px"
+            >
+              경기 화성시 병점로 12
+            </q-item-label>
+            <q-item-label
+              class="text-bold"
+              caption=""
+              lines="2"
+              style="color: #999999; font-size: 12px"
+            >
+              <span class="review-matching-text-border">
+                <span class="text-secondary notosanskr-medium">1,520 </span>
+                <span class="notosanskr-regular">리뷰</span>
+              </span>
+              <span class="review-matching-text">
+                <span class="text-secondary notosanskr-medium">12,520 </span>
+                <span class="notosanskr-regular">매칭</span></span
+              >
+            </q-item-label>
+          </q-item-section>
+          <q-item-section side="" top="">
+            <span>
+              <q-icon name="star" color="secondary" style="font-size:20px">
+              </q-icon>
+              <q-item-label
+                class="notosanskr-medium score-value text-black text-center"
+              >
+                4.9
+              </q-item-label>
+            </span>
+          </q-item-section>
+        </q-item>
+        <q-separator :key="count + '-separator'" spaced=""></q-separator>
+      </template>
+    </q-list>
+  </div>
 </template>
 
 <script>
+import SearchFilter from "components/SearchPage/SearchFilter.vue";
 export default {
-  name: 'Search',
-  data () {
-    return {
-      slide: 1
-    }
-  },
+  name: "Search",
   components: {
-
+    "search-filter": SearchFilter,
+  },
+  data() {
+    return {
+      slide: 1,
+    };
   },
   methods: {
-    onItemClick () {
-      alert('click');
+    onItemClick() {
+      alert("click");
     },
-    onItemResultClick () {
-      this.$router.push("store")
+    onItemResultClick() {
+      this.$router.push("store");
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="stylus">
