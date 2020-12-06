cafb673df65d0d137f6d57e046d535517335c9f2
eevan7a9 <e7a9@protonmail.com>
2020-12-06 17:58:44 +0900

    perf(StoreLayout): restructure w/ components

--- src/layouts/StoreLayout.vue
+++ src/layouts/StoreLayout.vue
@@ -2,27 +2,7 @@
   <default-template :styleSlots="styleSlots">
     <template #pageHeader>
       <nav-header-info :name="'어게인업'" class="full-width" backTo="search" />
-      <q-tabs
-        v-model="filterTabs"
-        class="text-black notosanskr-regular"
-        active-color="secondary"
-        dense
-        align="justify"
-      >
-        <q-route-tab
-          name="store_detail"
-          :to="{ name: 'store_detail' }"
-          class="filterTab"
-          label="업체소개"
-        />
-        <q-route-tab
-          name="reviews"
-          :to="{ name: 'store_review' }"
-          class="filterTab"
-          label="후기(633)"
-        />
-        <q-route-tab name="inquire" :to="{ name: 'store_inquiry' }" class="filterTab" label="문의하기" />
-      </q-tabs>
+      <top-toolbar-tabs />
     </template>
 
     <template #pageBody>
@@ -30,14 +10,7 @@
     </template>
 
     <template #pageFooter>
-      <div class="full-width notosanskr-medium">
-        <!-- Request Quote Button -->
-        <q-btn flat align="between" class="btn-request-quote row full-width">
-          <q-icon name="favorite_border" style="width: 75px"></q-icon>
-          <q-separator vertical inset></q-separator>
-          <div class="col">견적신청</div>
-        </q-btn>
-      </div>
+      <estimate-quote-request></estimate-quote-request>
     </template>
   </default-template>
 </template>
@@ -45,11 +18,15 @@
 <script>
 import DefaultTemplate from "src/components/TemplateFrames/DefaultTemplate";
 import NavHeaderInfo from "src/components/Utility/NavHeaderInfo";
+import EstimatedQuoteRequest from "src/components/StoreLayoutComponents/EstimatedQuoteRequest";
+import TopToolbarTabs from "src/components/StoreLayoutComponents/TopToolbarTabs";
 export default {
   name: "StoreLayout",
   components: {
     "default-template": DefaultTemplate,
     "nav-header-info": NavHeaderInfo,
+    "estimate-quote-request": EstimatedQuoteRequest,
+    "top-toolbar-tabs": TopToolbarTabs,
   },
   data() {
     return {
@@ -58,22 +35,9 @@ export default {
         bgBody: "#F3F3F3",
         bgFooter: "",
       },
-      filterTabs: null,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.btn-request-quote {
-  height: 60px;
-  background: #2cb3ff;
-
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 60px;
-  text-align: center;
-  letter-spacing: -0.8px;
-  color: #ffffff;
-}
-</style>
+<style lang="scss" scoped></style>
