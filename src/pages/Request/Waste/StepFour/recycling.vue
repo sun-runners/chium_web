2edbee36aa7918a5733cdb6bd300bf992046aa1b
eevan7a9 <e7a9@protonmail.com>
2020-12-18 13:45:38 +0900

    feat(recycling): created step 4 due-date

---
+++ src/pages/Request/Waste/StepFour/recycling.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <work-due-date @next="(val) => $emit('next', val)" />
+  </div>
+</template>
+
+<script>
+import { WorkDueDate } from "src/components/RequestEstimate/Common";
+export default {
+  components: {
+    WorkDueDate,
+  },
+  computed: {
+    layoutState() {
+      return {
+        totalSteps: 6,
+        activeStep: 4,
+        btnLabel: "다음",
+        isBtnReady: false,
+        isBtnHidden: false,
+        requestComplete: false,
+        nextPathRoute: "/request/waste/five/recycling",
+        prevPathRoute: "/request/waste/three/recycling",
+      };
+    },
+  },
+  mounted() {
+    this.$emit("setStateLayout", this.layoutState);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.heading-title {
+  font-size: 27px;
+  line-height: 38px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -1.35px;
+  color: #15161a;
+  padding-bottom: 36px;
+}
+.heading-subtitle {
+  color: #959595;
+}
+.schedule-undecided {
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: -0.8px;
+  color: #15161a;
+}
+</style>
