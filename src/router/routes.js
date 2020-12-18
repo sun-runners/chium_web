96971ad91c8710ef61931444d571644ed223db83
eevan7a9 <e7a9@protonmail.com>
2020-12-18 12:00:13 +0900

    perf(routes): added recycling step 2

--- src/router/routes.js
+++ src/router/routes.js
@@ -29,6 +29,7 @@ const routes = [
       { path: 'waste/two/household', component: () => import('pages/Request/Waste/StepTwo/household.vue') },
       { path: 'waste/two/workplace', component: () => import('pages/Request/Waste/StepTwo/workplace.vue') },
       { path: 'waste/two/construction', component: () => import('pages/Request/Waste/StepTwo/construction.vue') },
+      { path: 'waste/two/recycling', component: () => import('pages/Request/Waste/StepTwo/recycling.vue') },
       // step Three
       { path: 'waste/three/household', component: () => import('pages/Request/Waste/StepThree/household.vue') },
       { path: 'waste/three/workplace', component: () => import('pages/Request/Waste/StepThree/workplace.vue') },
