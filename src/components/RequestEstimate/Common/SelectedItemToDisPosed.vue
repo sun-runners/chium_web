2e7a1388d44ce86ddda1128c89aca33d6402cfc8
Reichard Cris <reichard@jdlife.co.nz>
2020-12-23 07:20:42 +0800

    Update SelectedItemToDisposedd

--- src/components/RequestEstimate/Common/SelectedItemToDisPosed.vue
+++ src/components/RequestEstimate/Common/SelectedItemToDisPosed.vue
@@ -1,90 +1,51 @@
 <template>
   <div class="">
     <p class="heading-title bg-white q-ma-none q-px-md notosanskr-regular">
-      공간 유형
-      <span class="heading-subtitle">을 알려주세요.</span>
+      상업공간 타입
+      <span class="heading-subtitle">을 골라주세요.</span>
     </p>
-    <div class="column full-height ">
-      <div class="notosanskr-regular bg-white q-px-md">
-        <div class="col-12 q-pa-none" style="padding-bottom: 24px">
-          <q-input
-            outlined
-            class="field"
-            ref="myItemField"
-            :label="label"
-            label-color="grey-13"
-            v-model="myItem"
-          >
-            <template v-slot:append>
-              <q-btn
-                flat
-                class="attachment notosanskr-medium"
-                @click="addToDiscarded"
-              >
-                등록
-              </q-btn>
-            </template>
-          </q-input>
-        </div>
-      </div>
-
-      <div
-        style="flex-grow:1;"
-        class="bg-white"
-        v-if="!discardedItems.length"
-      ></div>
-      <div class="q-mt-sm" style="background: #F3F3F3; flex-grow:1;" v-else>
-        <div class="discarded bg-white q-px-md q-py-lg notosanskr-regular">
-          <div class="heading-text notosanskr-medium">폐기품목</div>
-          <div class="row">
-            <div
-              class="row item q-pl-sm q-ma-xs"
-              v-for="(item, i) in discardedItems"
-              :key="i"
-            >
-              {{ item }}
-              <q-separator class="q-ml-xs" vertical inset=""></q-separator>
-              <q-btn
-                flat
-                color="grey-14"
-                padding="0 6px 0px 4px"
-                @click="removeFromDiscarded(item)"
-              >
-                <q-icon name="clear" size="16px"></q-icon>
-              </q-btn>
-            </div>
-          </div>
-        </div>
+    <div class="row q-col-gutter-md q-pa-md">
+      <div v-for="(list, key) in options" :key="key" class="col-6">
+        <q-btn class="full-width selected-btn items-center justify-center" flat>
+          {{ list }}
+        </q-btn>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data() {
+  data () {
     return {
-      myItem: "",
-      label: "옷장2개 (품목과 수량을 적어주세요.)",
+      myItem: '',
+      label: '',
       discardedItems: [],
-    };
+      options: [
+        '카페/식당',
+        '상가/매장',
+        '숙박/병원',
+        '학원/교육',
+        '사무실'
+      ]
+    }
   },
 
   methods: {
-    addToDiscarded() {
+    addToDiscarded () {
       if (this.myItem && !this.discardedItems.includes(this.myItem)) {
         this.discardedItems.push(this.myItem);
       }
       this.myItem = "";
       this.$refs.myItemField.blur();
     },
-    removeFromDiscarded(item) {
+    removeFromDiscarded (item) {
       if (this.discardedItems.includes(item)) {
         this.discardedItems = this.discardedItems.filter((i) => i != item);
       }
     },
   },
   watch: {
-    discardedItems(newVal) {
+    discardedItems (newVal) {
       this.$emit("next", true);
       if (!newVal.length) {
         this.$emit("next", false);
@@ -107,38 +68,13 @@ export default {
 .heading-subtitle {
   color: #959595;
 }
-// input
-.field {
-  background: #ffffff;
+.selected-btn {
+  background: #FFFFFF;
+  border: 1px solid #D9D9D9;
   box-sizing: border-box;
-  border-radius: 8px;
-  .attachment {
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 24px;
-    text-align: center;
-    letter-spacing: -0.8px;
-    color: #195de4;
-  }
-}
-.discarded {
-  .heading-text {
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 24px;
-    letter-spacing: -0.8px;
-    color: #15161a;
-    padding-bottom: 20px;
-  }
-  .item {
-    font-size: 14px;
-    line-height: 36px;
-    text-align: center;
-    letter-spacing: -0.7px;
-    color: #15161a;
-
-    background: #f6f6f6;
-    border-radius: 8px;
-  }
+  border-radius: 1000px;
+  height: 48px;
+  font-size: 15px;
+  line-height: 48px;
 }
 </style>
