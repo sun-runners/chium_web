c061ad10e6bbc542392066440906a7a813a6bfec
eevan7a9 <e7a9@protonmail.com>
2020-12-22 19:35:01 +0900

    feat(BottomDialogs): created ChangeLocationQuest component

---
+++ src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeLocationQuest/Index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <q-btn
+      flat
+      class="change-btn notosanskr-medium"
+      padding="0px"
+      @click="dialog = true"
+    >
+      변경
+    </q-btn>
+    <q-dialog v-model="dialog" position="bottom">
+      <q-card class="notosanskr-regular">
+        <q-card-section
+          class="q-pa-none q-py-sm row items-center justify-between full-width"
+        >
+          <div class="heading-title q-pl-md notosanskr-medium">
+            의뢰장소 변경
+          </div>
+          <q-btn color="white" flat text-color="black" @click="dialog = false">
+            <q-icon name="close" />
+          </q-btn>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: "",
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.q-card {
+  width: 100%;
+  max-width: 1000px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  height: 100vh;
+}
+.heading-title {
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 22px;
+  letter-spacing: -0.75px;
+  color: #15161a;
+}
+.change-btn {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: right;
+  margin: 0px;
+  letter-spacing: -0.8px;
+  padding: 0 16px;
+  color: #195de4;
+}
+</style>
