3cb3b925fd262439274f0f42318d2c347af39119
eevan7a9 <e7a9@protonmail.com>
2020-12-23 17:03:01 +0900

    perf(RecentPlaces): change whole Item as btn

--- src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeLocationQuest/RecentPlaces.vue
+++ src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeLocationQuest/RecentPlaces.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="full-height">
-    <div
+  <div class="item-container">
+    <q-btn
       v-for="(address, i) of recentAddress"
       :key="i"
-      class="recent-place q-pl-md q-py-md notosanskr-regular"
+      flat
+      class="full-width notosanskr-regular"
+      dense
+      padding="16px"
+      @click="setNewAddress(address)"
     >
-      <q-badge
-        outline
-        :color="currentAddress == address ? 'secondary' : 'grey'"
-        class="q-mb-sm"
-        text-color="black"
-        label="현재장소"
-      />
-      <div class="row justify-between">
-        <!-- address text | 주소 텍스트 -->
-        <div class="place-text">
-          {{ address }}
-        </div>
-        <!-- check icon | 확인 아이콘 -->
-        <q-btn
-          flat
-          color="white"
+      <div class="full-width text-left">
+        <q-badge
+          outline
+          :color="currentAddress == address ? 'secondary' : 'grey'"
+          class="q-mb-sm"
           text-color="black"
-          @click="setNewAddress(address)"
-        >
-          <q-icon size="24px">
-            <img
-              src="~assets/circle_check_blue.svg"
-              v-if="newAddress == address"
-            />
-            <img src="~assets/circle_check_grey.svg" v-else />
-          </q-icon>
-        </q-btn>
+          label="현재장소"
+        />
+        <div class="row justify-between">
+          <!-- address text | 주소 텍스트 -->
+          <div class="place-text">
+            {{ address }}
+          </div>
+          <!-- check icon | 확인 아이콘 -->
+          <q-btn flat color="white" text-color="black">
+            <q-icon size="24px">
+              <img
+                src="~assets/circle_check_blue.svg"
+                v-if="newAddress == address"
+              />
+              <img src="~assets/circle_check_grey.svg" v-else />
+            </q-icon>
+          </q-btn>
+        </div>
       </div>
-    </div>
+    </q-btn>
   </div>
 </template>
 
@@ -47,8 +48,17 @@ export default {
       recentAddress: [
         "경기도 치움시 철거동 123-4 치움 번지 폐기 타운하우스 112호 (15812)",
         "서울특별시 성북구 길음로 334 (길음동,길음뉴타운) 길음 뉴타운 8단지 812동 1902호(20202)",
-        "경기도 치움시 철거동 13-4 치움 번지 폐기 타운하우스 112호 (15812)",
-        "경기도 치움시 철거동 23-4 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 13-3 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 23-6 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 13-7 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 23-8 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 13-9 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 23-40 4 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 22 치움 번지 폐기 타운하우스 112호 (15812)",
+        "경기도 치움시 철거동 1 4 치움 번지 폐기 타운하우스 112호 (15812)",
+        "1 경기도 치움시 철거동 2d4 치움 번지 폐기 타운하우스 112호 (15812)",
+        "2 경기도 치움시 철거동 -4 치움 번지 폐기 타운하우스 112호 (15812)",
+        "3 경기도 치움시 철거동 2s 치움 번지 폐기 타운하우스 112호 (15812)",
       ],
     };
   },
