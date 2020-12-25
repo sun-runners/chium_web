ae0369185dee2cd7172ac21bad531ce56c46fc96
eevan7a9 <e7a9@protonmail.com>
2020-12-25 11:33:53 +0900

    perf(ChangeElevator): added options content

--- src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeElevator.vue
+++ src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeElevator.vue
@@ -14,7 +14,7 @@
         <q-card-section
           class="q-pa-none q-py-sm row items-center justify-between full-width"
         >
-          <div class="heading-title q-pl-md notosanskr-medium">
+          <div class="toolbar-title q-pl-md notosanskr-medium">
             엘레베이터 변경
           </div>
           <q-btn color="white" flat text-color="black" @click="dialog = false">
@@ -22,12 +22,43 @@
           </q-btn>
         </q-card-section>
 
-        <!-- wheel picker section | 휠 선택기 섹션-->
+        <!-- content section | 콘텐츠 섹션 -->
         <q-card-section
-          class="flex-grow full-height full-width q-pa-none row justify-center items-center"
+          class="flex-grow full-height full-width q-pa-none q-pt-md"
           style="padding-bottom: 170px;"
         >
-          <h4>To be develop</h4>
+          <div style="width: 343px">
+            <p class="heading-title bg-white q-ma-none q-px-md block">
+              엘레베이터 유무
+              <span class="heading-subtitle">에 대해서 알려주세요.</span>
+            </p>
+          </div>
+          <div v-for="(option, i) in elevatorOptions" :key="i">
+            <q-btn
+              flat
+              class="options-text full-width"
+              style="height: 64px;"
+              @click="selectedOption = option"
+            >
+              <div class="full-width text-left">
+                <q-icon
+                  style="margin-right: 20px"
+                  v-if="selectedOption == option"
+                >
+                  <img
+                    src="~assets/request_estimate-icon/radio-btn-selected.png"
+                  />
+                </q-icon>
+                <q-icon style="margin-right: 20px" v-else>
+                  <img
+                    src="~assets/request_estimate-icon/radio-btn-unselected.png"
+                  />
+                </q-icon>
+                {{ option }}
+              </div>
+            </q-btn>
+            <q-separator inset="" v-if="i == 0"></q-separator>
+          </div>
         </q-card-section>
 
         <!-- Footer section | 바닥 글 버튼 섹션  -->
@@ -53,6 +84,9 @@ export default {
     return {
       dialog: false,
       changeEnable: true,
+
+      elevatorOptions: ["있음", "없음"],
+      selectedOption: "있음",
     };
   },
   methods: {},
@@ -77,12 +111,24 @@ export default {
   border-top-right-radius: 16px;
   height: 558px;
 }
-.heading-title {
+.toolbar-title {
   font-weight: 500;
   font-size: 15px;
   line-height: 22px;
   letter-spacing: -0.75px;
   color: #15161a;
+}
+.heading-title {
+  font-size: 27px;
+  line-height: 38px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -1.35px;
+  color: #15161a;
+  padding-bottom: 24px;
+}
+.heading-subtitle {
+  color: #959595;
 }
 .change-btn {
   font-weight: 500;
