f9dfe2930f6be37e87206a284dabff1397a187b9
eevan7a9 <e7a9@protonmail.com>
2020-12-25 19:44:22 +0900

    feat(RequestDetails): added Channge Schedule Work

---
+++ src/components/RequestEstimate/FinalStep/BottomDialogs/ChangeScheduleWork.vue
@@ -0,0 +1,97 @@
+<template>
+  <div>
+    <q-btn
+      flat
+      class="change-btn notosanskr-medium"
+      padding="0px"
+      @click="dialog = true"
+    >
+      변경
+    </q-btn>
+    <q-dialog v-model="dialog" position="bottom">
+      <q-card class="notosanskr-regular">
+        <q-card-section
+          class="q-pa-none q-py-sm row items-center justify-between full-width"
+        >
+          <div class="toolbar-title q-pl-md notosanskr-medium">
+            작업 예정일 변경
+          </div>
+          <q-btn color="white" flat text-color="black" @click="dialog = false">
+            <q-icon name="close" />
+          </q-btn>
+        </q-card-section>
+
+        <q-card-section class="notosanskr-medium">
+          <div class="heading-title">
+            작업 예정일
+            <span class="heading-subtitle">을 알려주세요.</span>
+          </div>
+        </q-card-section>
+
+        <!-- Footer section | 바닥 글 버튼 섹션  -->
+        <q-card-section class="q-pa-xs q-px-md absolute-bottom">
+          <q-btn
+            style="height: 60px;"
+            :color="changeEnable ? 'secondary' : 'grey'"
+            class="change-btn full-width notosanskr-medium "
+            unelevated
+            text-color="white"
+            label="변경하기"
+          />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      changeEnable: true,
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.q-card {
+  width: 100%;
+  max-width: 1000px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  height: 100vh;
+}
+.toolbar-title {
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 22px;
+  letter-spacing: -0.75px;
+  color: #15161a;
+}
+.heading-title {
+  font-size: 27px;
+  line-height: 38px;
+  display: flex;
+  align-items: center;
+  letter-spacing: -1.35px;
+  color: #15161a;
+  padding-bottom: 24px;
+}
+.heading-subtitle {
+  color: #959595;
+}
+.change-btn {
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  text-align: right;
+  margin: 0px;
+  letter-spacing: -0.8px;
+  padding: 0 16px;
+  color: #195de4;
+}
+</style>
