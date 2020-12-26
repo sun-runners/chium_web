d7bc05defbf531913c9d1677647a5448ce8d3224
Sunwook KIm <sunus7@naver.com>
2020-12-26 21:46:11 +0900

    라우팅 막기

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,8 +24,16 @@
               style="color: #999999"
               label="검색"
               icon="find_in_page"
+              @click="notReady"
             ></q-route-tab>
-            <!-- <q-route-tab name="tab4" to="/store" style="color: #999999" label="업체" icon="store"></q-route-tab> -->
+            <q-route-tab
+              name="tab4"
+              to="/store"
+              style="color: #999999"
+              label="업체"
+              icon="store"
+              @click="notReady"
+            ></q-route-tab>
             <q-route-tab
               name="tab5"
               to="/my_page"
@@ -54,58 +62,78 @@
         </div>
       </transition-group>
     </q-page-container>
+
+    <q-dialog v-model="dialogNotReady">
+      <q-card align="center">
+        <q-card-section>
+          <div class="text-h5">준비중인 페이지입니다.</div>
+        </q-card-section>
+        <q-card-section>
+          고객님, 죄송합니다.
+          현재는 베타서비스(1월 ~ 3월) 기간이며, 4월 초로 정식 런칭예정입니다.
+        </q-card-section>
+        <q-card-actions align="center">
+          <q-btn label="확인" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-layout>
 </template>
 
 <script>
-import { get, cloneDeep } from "lodash";
-import Navigation from "src/components/NavHeader/Navigation";
+import { get, cloneDeep } from 'lodash'
+import Navigation from 'src/components/NavHeader/Navigation'
 
 export default {
   components: {
-    Navigation,
+    Navigation
   },
-  data() {
+  data () {
     return {
-      heading_name: "0",
+      heading_name: '0',
       window_height: window.innerHeight,
       widnow_width: window.innerWidth,
       contentStyle: {
-        backgroundColor: "rgba(0,0,0,0.02)",
-        color: "#555",
+        backgroundColor: 'rgba(0,0,0,0.02)',
+        color: '#555'
       },
       contentActiveStyle: {
-        backgroundColor: "#eee",
-        color: "black",
+        backgroundColor: '#eee',
+        color: 'black'
       },
       thumbStyle: {
-        right: "2px",
-        borderRadius: "5px",
-        backgroundColor: "#027be3",
-        width: "5px",
-        opacity: 0.75,
+        right: '2px',
+        borderRadius: '5px',
+        backgroundColor: '#027be3',
+        width: '5px',
+        opacity: 0.75
       },
-    };
+      dialogNotReady: false
+    }
   },
-  created() {
-    console.log(this);
-    window.addEventListener("resize", this.handleHeight);
+  created () {
+    console.log(this)
+    window.addEventListener('resize', this.handleHeight)
   },
-  destroyed() {
-    window.removeEventListener("resize", this.handleHeight);
+  destroyed () {
+    window.removeEventListener('resize', this.handleHeight)
   },
   methods: {
-    handleHeight() {
-      this.window_height = window.innerHeight;
-      this.widnow_width = window.innerWidth;
+    handleHeight () {
+      this.window_height = window.innerHeight
+      this.widnow_width = window.innerWidth
     },
+    notReady (e, go) {
+      e.navigate = false
+      this.dialogNotReady = true
+    }
   },
   computed: {
-    widthMax() {
-      return { width: this.widnow_width + "px", "max-width": "1000px" };
-    },
-  },
-};
+    widthMax () {
+      return { width: this.widnow_width + 'px', 'max-width': '1000px' }
+    }
+  }
+}
 </script>
 
 <style lang="stylus">
