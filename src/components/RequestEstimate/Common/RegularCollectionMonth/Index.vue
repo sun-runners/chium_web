e50c64235b5ac3147fb3b278fe1aa9de52d64d58
eevan7a9 <e7a9@protonmail.com>
2021-03-25 17:32:58 +0900

    perf(WeekSelection): enable wheel select week

--- src/components/RequestEstimate/Common/RegularCollectionMonth/Index.vue
+++ src/components/RequestEstimate/Common/RegularCollectionMonth/Index.vue
@@ -10,18 +10,7 @@
         <div class="item-header notosanskr-regular">
           매월 몇번째 주
         </div>
-
-        <q-btn
-          class="item-value full-width notosanskr-regular"
-          align="between"
-          flat
-          padding="2px"
-          @click="weekDialog = !weekDialog"
-        >
-          첫번째
-          <q-icon name="keyboard_arrow_down" size="24px" />
-        </q-btn>
-
+        <week-selection @selectedValue="(e) => (week = e)" />
         <q-separator class="q-mt-sm" style="background: #5A5A5A;" size="2px" />
       </section>
 
@@ -40,76 +29,21 @@
           월요일
           <q-icon name="keyboard_arrow_down" size="24px" />
         </q-btn>
-
         <q-separator class="q-mt-sm" style="background: #5A5A5A;" size="2px" />
       </section>
     </div>
-    <!---------------------------------- BOTTOM DIALOGS ----------------------------->
-    <q-dialog v-model="weekDialog" position="bottom">
-      <q-card class="row overflow-hidden notosanskr-regular">
-        <q-card-section
-          class="q-pa-none q-py-sm row items-center justify-between full-width bg-white"
-        >
-          <div class="toolbar-title q-pl-md notosanskr-medium">
-            몇째 주
-          </div>
-          <q-btn
-            color="white"
-            flat
-            text-color="black"
-            @click="weekDialog = false"
-          >
-            <q-icon name="close" />
-          </q-btn>
-        </q-card-section>
-        <q-separator color="grey-12" />
-
-        <q-card-section
-          class="flex-grow full-height bg-white full-width q-pa-none row justify-center items-center"
-          style="padding-bottom: 170px;"
-        >
-          <div class="row justify-center items-center">
-            <div class="" style=" width: 250px">
-              <custom-wheel-picker
-                v-model="selectedWeek"
-                :options="week"
-              ></custom-wheel-picker>
-            </div>
-
-            <div class="layer">
-              주
-            </div>
-          </div>
-        </q-card-section>
-
-        <!-- Footer section | 바닥 글 버튼 섹션  -->
-        <q-card-section class="absolute-bottom q-px-none">
-          <q-btn
-            style="height: 60px;"
-            :color="selectedWeek != null ? 'secondary' : 'grey'"
-            :disable="selectedWeek == null"
-            class="change-btn full-width notosanskr-medium "
-            unelevated
-            text-color="white"
-            label="완료"
-          />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
 
 <script>
-import CustomWheelPicker from "components/Utility/CustomWheelPicker/Index";
+import WeekSelection from "./WeekSelection.vue";
 export default {
   components: {
-    CustomWheelPicker,
+    WeekSelection,
   },
   data() {
     return {
-      week: ["네번째", "첫번째", "두번째"],
-      selectedWeek: "첫번째",
-      weekDialog: true,
+      week: "",
     };
   },
 };
@@ -141,23 +75,6 @@ export default {
   font-size: 22px;
   line-height: 40px;
   letter-spacing: -1.1px;
-  color: #15161a;
-}
-/** ------------- DIALOG ---------------- */
-.q-card {
-  width: 100%;
-  max-width: 1000px;
-  border-top-left-radius: 16px;
-  border-top-right-radius: 16px;
-  height: 60vh;
-
-  background: #f3f3f3;
-}
-.toolbar-title {
-  font-weight: 500;
-  font-size: 15px;
-  line-height: 22px;
-  letter-spacing: -0.75px;
   color: #15161a;
 }
 </style>
---
+++ src/components/RequestEstimate/Common/RegularCollectionMonth/WeekSelection.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <q-btn
+      class="item-value full-width notosanskr-regular"
+      align="between"
+      flat
+      padding="2px"
+      @click="weekDialog = !weekDialog"
+    >
+      {{ week }}
+      <q-icon name="keyboard_arrow_down" size="24px" />
+    </q-btn>
+    <q-dialog v-model="weekDialog" position="bottom">
+      <q-card class="row overflow-hidden notosanskr-regular">
+        <q-card-section
+          class="q-pa-none q-py-sm row items-center justify-between full-width bg-white"
+        >
+          <div class="toolbar-title q-pl-md notosanskr-medium">
+            몇째 주
+          </div>
+          <q-btn
+            color="white"
+            flat
+            text-color="black"
+            @click="weekDialog = false"
+          >
+            <q-icon name="close" />
+          </q-btn>
+        </q-card-section>
+        <q-separator color="grey-12" />
+
+        <q-card-section
+          class="flex-grow full-height bg-white full-width q-pa-none row justify-center items-center"
+          style="padding-bottom: 170px;"
+        >
+          <div class="row justify-center items-center">
+            <div class="" style=" width: 250px">
+              <custom-wheel-picker
+                v-model="wheelSelected"
+                :options="wheelSelection"
+              ></custom-wheel-picker>
+            </div>
+
+            <div class="layer">
+              주
+            </div>
+          </div>
+        </q-card-section>
+
+        <!-- Footer section | 바닥 글 버튼 섹션  -->
+        <q-card-section class="absolute-bottom q-px-none">
+          <q-btn
+            style="height: 60px;"
+            :color="wheelSelected != null ? 'secondary' : 'grey'"
+            :disable="wheelSelected == null"
+            class="change-btn full-width notosanskr-medium"
+            unelevated
+            text-color="white"
+            label="완료"
+            @click="complete"
+          />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import CustomWheelPicker from "components/Utility/CustomWheelPicker/Index";
+
+export default {
+  components: {
+    CustomWheelPicker,
+  },
+  data() {
+    return {
+      wheelSelection: ["네번째", "첫번째", "두번째"],
+      wheelSelected: "",
+      weekDialog: true,
+      week: "첫번째",
+    };
+  },
+  mounted() {
+    this.$emit("selectedValue", this.week);
+    this.wheelSelected = this.week;
+  },
+  methods: {
+    complete() {
+      this.week = this.wheelSelected;
+      this.$emit("selectedValue", this.week);
+      this.weekDialog = false;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.item-value {
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 40px;
+  letter-spacing: -1.1px;
+  color: #15161a;
+}
+/** ------------- DIALOG ---------------- */
+.q-card {
+  width: 100%;
+  max-width: 1000px;
+  border-top-left-radius: 16px;
+  border-top-right-radius: 16px;
+  height: 60vh;
+
+  background: #f3f3f3;
+}
+.toolbar-title {
+  font-weight: 500;
+  font-size: 15px;
+  line-height: 22px;
+  letter-spacing: -0.75px;
+  color: #15161a;
+}
+</style>
